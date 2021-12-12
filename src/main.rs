eda4320238eae5f0355711321d958c64a1160e28
Casey Rodarmor <casey@rodarmor.com>
2021-12-12 01:30:50 -0800

    Track atom locations

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use bitcoincore_rpc::{Auth, Client, RpcApi};
+use {
+  bitcoin::blockdata::transaction::OutPoint,
+  bitcoincore_rpc::{Auth, Client, RpcApi},
+  std::collections::BTreeMap,
+};
 
 type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
 
@@ -19,8 +23,57 @@ fn main() -> Result<()> {
 
   let client = Client::new("http://localhost:8332", Auth::CookieFile(cookiefile))?;
 
-  let best_block_hash = client.get_best_block_hash().unwrap();
-  println!("best block hash: {}", best_block_hash);
+  let tip_height = client
+    .get_block_header_info(&client.get_best_block_hash()?)?
+    .height as u64;
+
+  eprintln!("Scanning for atoms up to height {}…", tip_height);
+
+  let mut atoms: BTreeMap<OutPoint, u64> = BTreeMap::new();
+
+  for height in 0..tip_height {
+    eprintln!("Scanning for atoms in block {}…", height);
+    let hash = client.get_block_hash(height)?;
+
+    let block = client.get_block(&hash)?;
+
+    for (i, transaction) in block.txdata.iter().enumerate() {
+      if i == 0 {
+        atoms.insert(
+          OutPoint {
+            txid: transaction.txid(),
+            vout: 0,
+          },
+          height,
+        );
+      } else {
+        let transferred = transaction
+          .input
+          .iter()
+          .map(|txin| atoms.remove(&txin.previous_output))
+          .flatten()
+          .collect::<Vec<u64>>();
+
+        if !transferred.is_empty() {
+          eprintln!(
+            "Transferring {} atoms: {:?}",
+            transferred.len(),
+            transferred
+          );
+        }
+
+        let value = transaction
+          .output
+          .iter()
+          .map(|txout| txout.value)
+          .sum::<u64>();
+
+        for (vout, output) in transaction.output.iter().enumerate() {
+          todo!("Transfer atoms proportionally according to output value");
+        }
+      }
+    }
+  }
 
   Ok(())
 }
