b52044b3a63e571c3b0a3fe07282cc630beb36fc
Christopher Berner <christopherberner@gmail.com>
2022-01-05 21:29:43 -0800

    Rename a bunch of variables

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,10 +1,10 @@
 use std::path::PathBuf;
 use super::*;
 
-const DESCENDENTS: &str = "descendents";
+const CHILDREN: &str = "children";
 const HEIGHTS: &str = "heights";
 const BLOCK_OFFSETS: &str = "block_offsets";
-const HASHES: &str = "hashes";
+const HEIGHTS_TO_HASHES: &str = "height_to_hashes";
 
 #[derive(StructOpt)]
 pub enum Arguments {
@@ -25,7 +25,7 @@ impl Arguments {
         {
           let tx = db.begin_write()?;
 
-          let mut descendents: MultimapTable<[u8], [u8]> = tx.open_multimap_table(DESCENDENTS)?;
+          let mut children: MultimapTable<[u8], [u8]> = tx.open_multimap_table(CHILDREN)?;
 
           let mut block_offsets: Table<[u8], u64> = tx.open_table(BLOCK_OFFSETS)?;
 
@@ -53,12 +53,12 @@ impl Arguments {
 
             let block = Block::consensus_decode(bytes)?;
 
-            descendents.insert(
-              &block.header.prev_blockhash.deref(),
-              &block.block_hash().deref(),
+            children.insert(
+              &block.header.prev_blockhash,
+              &block.block_hash(),
             )?;
 
-            block_offsets.insert(block.block_hash().deref(), &(offset as u64))?;
+            block_offsets.insert(&block.block_hash(), &(offset as u64))?;
 
             count += 1;
           }
@@ -72,15 +72,15 @@ impl Arguments {
           let write = db.begin_write()?;
 
           let mut heights: Table<[u8], u64> = write.open_table(HEIGHTS)?;
-          let mut hashes: Table<u64, [u8]> = write.open_table(HASHES)?;
+          let mut heights_to_hashes: Table<u64, [u8]> = write.open_table(HEIGHTS_TO_HASHES)?;
 
           heights.insert(genesis_block(Network::Bitcoin).block_hash().deref(), &0)?;
-          hashes.insert(&0, genesis_block(Network::Bitcoin).block_hash().deref())?;
+          heights_to_hashes.insert(&0, genesis_block(Network::Bitcoin).block_hash().deref())?;
 
           let read = db.begin_read()?;
 
-          let descendents: ReadOnlyMultimapTable<[u8], [u8]> =
-            read.open_multimap_table(DESCENDENTS)?;
+          let children: ReadOnlyMultimapTable<[u8], [u8]> =
+            read.open_multimap_table(CHILDREN)?;
 
           let mut queue = vec![(
             genesis_block(Network::Bitcoin)
@@ -92,12 +92,12 @@ impl Arguments {
 
           while let Some((block, height)) = queue.pop() {
             heights.insert(block.as_ref(), &height)?;
-            hashes.insert(&height, block.as_ref())?;
+            heights_to_hashes.insert(&height, block.as_ref())?;
 
-            let mut iter = descendents.get(&block)?;
+            let mut iter = children.get(&block)?;
 
-            while let Some(descendent) = iter.next() {
-              queue.push((descendent.to_vec(), height + 1));
+            while let Some(child) = iter.next() {
+              queue.push((child.to_vec(), height + 1));
             }
           }
 
@@ -110,8 +110,8 @@ impl Arguments {
 
         let tx = db.begin_read()?;
 
-        let hashes: ReadOnlyTable<u64, [u8]> = tx.open_table(HASHES)?;
-        let guard = hashes.get(&height)?.unwrap();
+        let heights_to_hashes: ReadOnlyTable<u64, [u8]> = tx.open_table(HEIGHTS_TO_HASHES)?;
+        let guard = heights_to_hashes.get(&height)?.unwrap();
         let hash = guard.to_value();
 
         let offsets: ReadOnlyTable<[u8], u64> = tx.open_table(BLOCK_OFFSETS)?;
