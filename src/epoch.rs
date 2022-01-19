5403ba646b12adc81e73f91e6906bd74fc23e3d7
Casey Rodarmor <casey@rodarmor.com>
2022-01-19 15:29:59 -0800

    More tests and cleanup

--- src/epoch.rs
+++ src/epoch.rs
@@ -85,20 +85,17 @@ mod tests {
   use super::super::*;
 
   #[test]
-  fn supply() {
-    let mut mined = 0;
-
-    for height in 0.. {
-      let subsidy = Height(height).subsidy();
-
-      if subsidy == 0 {
-        break;
-      }
-
-      mined += subsidy;
-    }
-
-    assert_eq!(Ordinal::SUPPLY, mined);
+  fn starting_ordinal() {
+    assert_eq!(Epoch(0).starting_ordinal().unwrap(), 0);
+    assert_eq!(
+      Epoch(1).starting_ordinal().unwrap(),
+      Epoch(0).subsidy() * Epoch::BLOCKS
+    );
+    assert_eq!(
+      Epoch(2).starting_ordinal().unwrap(),
+      (Epoch(0).subsidy() + Epoch(1).subsidy()) * Epoch::BLOCKS
+    );
+    assert_eq!(Epoch(33).starting_ordinal(), None);
   }
 
   #[test]
@@ -114,7 +111,6 @@ mod tests {
 
     assert_eq!(Epoch::STARTING_ORDINALS, epoch_ordinals);
     assert_eq!(Epoch::STARTING_ORDINALS.len(), 33);
-    assert_eq!(Epoch(33).subsidy(), 0);
   }
 
   #[test]
@@ -123,5 +119,39 @@ mod tests {
     assert_eq!(Epoch(1).subsidy(), 2500000000);
     assert_eq!(Epoch(32).subsidy(), 1);
     assert_eq!(Epoch(33).subsidy(), 0);
+  }
+
+  #[test]
+  fn blocks() {
+    // c.f. https://github.com/bitcoin/bitcoin/blob/master/src/chainparams.cpp
+    assert_eq!(Epoch::BLOCKS, 210000);
+  }
+
+  #[test]
+  fn starting_height() {
+    assert_eq!(Epoch(0).starting_height(), 0);
+    assert_eq!(Epoch(1).starting_height(), Epoch::BLOCKS);
+    assert_eq!(Epoch(2).starting_height(), Epoch::BLOCKS * 2);
+  }
+
+  #[test]
+  fn from_height() {
+    assert_eq!(Epoch::from(Height(0)), 0);
+    assert_eq!(Epoch::from(Height(Epoch::BLOCKS)), 1);
+    assert_eq!(Epoch::from(Height(Epoch::BLOCKS) + 1), 1);
+  }
+
+  #[test]
+  fn from_ordinal() {
+    assert_eq!(Epoch::from(Ordinal::new(0)), 0);
+    assert_eq!(Epoch::from(Ordinal::new(1)), 0);
+    assert_eq!(Epoch::from(Epoch(1).starting_ordinal().unwrap()), 1);
+    assert_eq!(Epoch::from(Epoch(1).starting_ordinal().unwrap() + 1), 1);
+  }
+
+  #[test]
+  fn eq() {
+    assert_eq!(Epoch(0), 0);
+    assert_eq!(Epoch(100), 100);
   }
 }
