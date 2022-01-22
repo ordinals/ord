344350eca9ff70ee0c91e7339e16e93d86817a93
Casey Rodarmor <casey@rodarmor.com>
2022-01-21 16:25:20 -0800

    Refactor find command into struct

--- src/command.rs
+++ src/command.rs
@@ -9,15 +9,7 @@ mod traits;
 #[derive(StructOpt)]
 pub(crate) enum Command {
   Epochs,
-  Find {
-    #[structopt(long)]
-    blocksdir: Option<PathBuf>,
-    #[structopt(long)]
-    as_of_height: u64,
-    #[structopt(long)]
-    slot: bool,
-    ordinal: Ordinal,
-  },
+  Find(find::Find),
   Name {
     name: String,
   },
@@ -36,12 +28,7 @@ impl Command {
   pub(crate) fn run(self) -> Result<()> {
     match self {
       Self::Epochs => epochs::run(),
-      Self::Find {
-        as_of_height,
-        blocksdir,
-        ordinal,
-        slot,
-      } => find::run(blocksdir.as_deref(), ordinal, as_of_height, slot),
+      Self::Find(find) => find.run(),
       Self::Name { name } => name::run(&name),
       Self::Range { height, name } => range::run(height, name),
       Self::Supply => supply::run(),
--- src/command/find.rs
+++ src/command/find.rs
@@ -1,10 +1,53 @@
 use super::*;
 
-// todo:
-// 1.
-//    index multiple blockfiles, so we can find ordinals that aren't in the first blockfile
-// 2.
-//    doesn't follow susequent transactions
+#[derive(StructOpt)]
+pub(crate) struct Find {
+  #[structopt(long)]
+  blocksdir: Option<PathBuf>,
+  #[structopt(long)]
+  as_of_height: u64,
+  #[structopt(long)]
+  slot: bool,
+  ordinal: Ordinal,
+}
+
+impl Find {
+  pub(crate) fn run(self) -> Result<()> {
+    let index = Index::new(self.blocksdir.as_deref())?;
+
+    let creation_height = self.ordinal.height().n();
+    assert!(creation_height < 100);
+    let block = index.block(creation_height)?.unwrap();
+
+    let offset = self.ordinal.subsidy_position();
+    let mut satpoint = find_in_transaction(&block.txdata[0], offset);
+    let mut slot = (creation_height, 0, satpoint.outpoint.vout, offset);
+
+    for height in (creation_height + 1)..(self.as_of_height + 1) {
+      match index.block(height)? {
+        Some(block) => {
+          for (txindex, transaction) in block.txdata.iter().enumerate() {
+            for input in &transaction.input {
+              if input.previous_output == satpoint.outpoint {
+                satpoint = find_in_transaction(&transaction, satpoint.offset);
+                slot = (height, txindex, satpoint.outpoint.vout, satpoint.offset);
+              }
+            }
+          }
+        }
+        None => break,
+      }
+    }
+
+    if self.slot {
+      println!("{}.{}.{}.{}", slot.0, slot.1, slot.2, slot.3);
+    } else {
+      println!("{satpoint}");
+    }
+
+    Ok(())
+  }
+}
 
 struct SatPoint {
   outpoint: OutPoint,
@@ -32,45 +75,4 @@ fn find_in_transaction(tx: &Transaction, mut offset: u64) -> SatPoint {
   }
 
   panic!("Could not find ordinal in transaction!");
-}
-
-pub(crate) fn run(
-  blocksdir: Option<&Path>,
-  ordinal: Ordinal,
-  as_of_height: u64,
-  print_slot: bool,
-) -> Result<()> {
-  let index = Index::new(blocksdir)?;
-
-  let creation_height = ordinal.height().n();
-  assert!(creation_height < 100);
-  let block = index.block(creation_height)?.unwrap();
-
-  let offset = ordinal.subsidy_position();
-  let mut satpoint = find_in_transaction(&block.txdata[0], offset);
-  let mut slot = (creation_height, 0, satpoint.outpoint.vout, offset);
-
-  for height in (creation_height + 1)..(as_of_height + 1) {
-    match index.block(height)? {
-      Some(block) => {
-        for (txindex, transaction) in block.txdata.iter().enumerate() {
-          for input in &transaction.input {
-            if input.previous_output == satpoint.outpoint {
-              satpoint = find_in_transaction(&transaction, satpoint.offset);
-              slot = (height, txindex, satpoint.outpoint.vout, satpoint.offset);
-            }
-          }
-        }
-      }
-      None => break,
-    }
-  }
-
-  if print_slot {
-    println!("{}.{}.{}.{}", slot.0, slot.1, slot.2, slot.3);
-  } else {
-    println!("{satpoint}");
-  }
-
-  Ok(())
 }
