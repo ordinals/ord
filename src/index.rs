4520b3a03ca09efe715530013d270b43f3b657eb
Casey Rodarmor <casey@rodarmor.com>
2022-01-27 17:13:28 -0800

    Use table names consistently

--- src/index.rs
+++ src/index.rs
@@ -45,7 +45,8 @@ impl Index {
     let mut height = 0;
     while let Some(block) = self.block(height)? {
       let wtx = self.database.begin_write()?;
-      let mut utxords: Table<[u8], [u8]> = wtx.open_table(Self::OUTPOINT_TO_ORDINAL_RANGES)?;
+      let mut outpoint_to_ordinal_ranges: Table<[u8], [u8]> =
+        wtx.open_table(Self::OUTPOINT_TO_ORDINAL_RANGES)?;
 
       let mut coinbase_inputs = VecDeque::new();
 
@@ -59,7 +60,7 @@ impl Index {
         for input in &tx.input {
           let mut key = Vec::new();
           input.previous_output.consensus_encode(&mut key)?;
-          let ordinal_ranges = utxords.get(key.as_slice())?.unwrap();
+          let ordinal_ranges = outpoint_to_ordinal_ranges.get(key.as_slice())?.unwrap();
 
           for chunk in ordinal_ranges.to_value().chunks_exact(16) {
             let start = u64::from_le_bytes(chunk[0..8].try_into().unwrap());
@@ -90,7 +91,7 @@ impl Index {
           };
           let mut key = Vec::new();
           outpoint.consensus_encode(&mut key)?;
-          utxords.insert(&key, &ordinals)?;
+          outpoint_to_ordinal_ranges.insert(&key, &ordinals)?;
         }
         coinbase_inputs.extend(&input_ordinal_ranges);
       }
@@ -120,7 +121,7 @@ impl Index {
           };
           let mut key = Vec::new();
           outpoint.consensus_encode(&mut key)?;
-          utxords.insert(&key, &ordinals)?;
+          outpoint_to_ordinal_ranges.insert(&key, &ordinals)?;
         }
       }
 
@@ -135,10 +136,10 @@ impl Index {
     {
       let tx = self.database.begin_write()?;
 
-      let mut children: MultimapTable<[u8], [u8]> =
+      let mut hash_to_children: MultimapTable<[u8], [u8]> =
         tx.open_multimap_table(Self::HASH_TO_CHILDREN)?;
 
-      let mut block_offsets: Table<[u8], u64> = tx.open_table(Self::HASH_TO_OFFSET)?;
+      let mut hash_to_offset: Table<[u8], u64> = tx.open_table(Self::HASH_TO_OFFSET)?;
 
       let blocks = fs::read(self.blocksdir.join("blk00000.dat"))?;
 
@@ -155,9 +156,9 @@ impl Index {
 
         let block = Block::consensus_decode(&blocks[range.clone()])?;
 
-        children.insert(&block.header.prev_blockhash, &block.block_hash())?;
+        hash_to_children.insert(&block.header.prev_blockhash, &block.block_hash())?;
 
-        block_offsets.insert(&block.block_hash(), &(offset as u64))?;
+        hash_to_offset.insert(&block.block_hash(), &(offset as u64))?;
 
         offset = range.end;
 
@@ -172,15 +173,15 @@ impl Index {
     {
       let write = self.database.begin_write()?;
 
-      let mut heights: Table<[u8], u64> = write.open_table(Self::HASH_TO_HEIGHT)?;
-      let mut heights_to_hashes: Table<u64, [u8]> = write.open_table(Self::HEIGHT_TO_HASH)?;
+      let mut hash_to_height: Table<[u8], u64> = write.open_table(Self::HASH_TO_HEIGHT)?;
+      let mut height_to_hash: Table<u64, [u8]> = write.open_table(Self::HEIGHT_TO_HASH)?;
 
-      heights.insert(genesis_block(Network::Bitcoin).block_hash().deref(), &0)?;
-      heights_to_hashes.insert(&0, genesis_block(Network::Bitcoin).block_hash().deref())?;
+      hash_to_height.insert(genesis_block(Network::Bitcoin).block_hash().deref(), &0)?;
+      height_to_hash.insert(&0, genesis_block(Network::Bitcoin).block_hash().deref())?;
 
       let read = self.database.begin_read()?;
 
-      let children: ReadOnlyMultimapTable<[u8], [u8]> =
+      let hash_to_children: ReadOnlyMultimapTable<[u8], [u8]> =
         read.open_multimap_table(Self::HASH_TO_CHILDREN)?;
 
       let mut queue = vec![(
@@ -192,10 +193,10 @@ impl Index {
       )];
 
       while let Some((block, height)) = queue.pop() {
-        heights.insert(block.as_ref(), &height)?;
-        heights_to_hashes.insert(&height, block.as_ref())?;
+        hash_to_height.insert(block.as_ref(), &height)?;
+        height_to_hash.insert(&height, block.as_ref())?;
 
-        let mut iter = children.get(&block)?;
+        let mut iter = hash_to_children.get(&block)?;
 
         while let Some(child) = iter.next() {
           queue.push((child.to_vec(), height + 1));
@@ -211,15 +212,15 @@ impl Index {
   pub(crate) fn block(&self, height: u64) -> Result<Option<Block>> {
     let tx = self.database.begin_read()?;
 
-    let heights_to_hashes: ReadOnlyTable<u64, [u8]> = tx.open_table(Self::HEIGHT_TO_HASH)?;
+    let heights_to_hash: ReadOnlyTable<u64, [u8]> = tx.open_table(Self::HEIGHT_TO_HASH)?;
 
-    match heights_to_hashes.get(&height)? {
+    match heights_to_hash.get(&height)? {
       None => Ok(None),
       Some(guard) => {
         let hash = guard.to_value();
 
-        let offsets: ReadOnlyTable<[u8], u64> = tx.open_table(Self::HASH_TO_OFFSET)?;
-        let offset = offsets.get(hash)?.unwrap().to_value() as usize;
+        let hash_to_offset: ReadOnlyTable<[u8], u64> = tx.open_table(Self::HASH_TO_OFFSET)?;
+        let offset = hash_to_offset.get(hash)?.unwrap().to_value() as usize;
 
         let blocks = fs::read(self.blocksdir.join("blk00000.dat"))?;
 
@@ -246,10 +247,11 @@ impl Index {
 
   pub(crate) fn list(&self, outpoint: OutPoint) -> Result<Vec<(u64, u64)>> {
     let rtx = self.database.begin_read()?;
-    let utxords: ReadOnlyTable<[u8], [u8]> = rtx.open_table(Self::OUTPOINT_TO_ORDINAL_RANGES)?;
+    let outpoint_to_oridnal_ranges: ReadOnlyTable<[u8], [u8]> =
+      rtx.open_table(Self::OUTPOINT_TO_ORDINAL_RANGES)?;
     let mut key = Vec::new();
     outpoint.consensus_encode(&mut key)?;
-    let ordinal_ranges = utxords.get(key.as_slice())?.unwrap();
+    let ordinal_ranges = outpoint_to_oridnal_ranges.get(key.as_slice())?.unwrap();
     let mut output = Vec::new();
     for chunk in ordinal_ranges.to_value().chunks_exact(16) {
       let start = u64::from_le_bytes(chunk[0..8].try_into().unwrap());
