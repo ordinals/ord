53331f67d1babb4d6552bbfd8c83a71864837d47
Liam <liam@scalzulli.com>
2022-02-06 14:25:35 -0500

    Use struct for block_with_coinbase

--- tests/list.rs
+++ tests/list.rs
@@ -80,8 +80,14 @@ fn duplicate_transaction_range() -> Result {
     .command(
       "list --blocksdir blocks d63a320a4b404d7933ca788e8f185f10e31e03bf6ab9fa4595bfedc2fcc5a4a8:0",
     )
-    .block_with_coinbase(true, false, 50 * COIN_VALUE)
-    .block_with_coinbase(true, false, 50 * COIN_VALUE)
+    .block_with_coinbase(Coinbase {
+      include_height: false,
+      ..Default::default()
+    })
+    .block_with_coinbase(Coinbase {
+      include_height: false,
+      ..Default::default()
+    })
     .block()
     .transaction(&[(0, 0, 0)], 1)
     .expected_stdout("[5000000000,10000000000)\n")
@@ -94,7 +100,10 @@ fn underpay_subsidy() -> Result {
     .command(
       "list --blocksdir blocks 12d57183977a1df616bafbb7dafbb4249e59d8f796ba556ad6bb75f0fa9fe0ea:0",
     )
-    .block_with_coinbase(true, true, 50 * COIN_VALUE - 1)
+    .block_with_coinbase(Coinbase {
+      subsidy: 50 * COIN_VALUE - 1,
+      ..Default::default()
+    })
     .expected_stdout("[0,4999999999)\n")
     .run()
 }
