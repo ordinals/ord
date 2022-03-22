1c7c0cc798d7684938916dc1dc2d0adc58192c63
Liam <liam@scalzulli.com>
2022-03-21 20:02:57 -0400

    Tweak

--- docs/index.js
+++ docs/index.js
@@ -1,15 +1,15 @@
-const list = (outpoint) => {
+function list(outpoint) {
   return fetch(`http://api.ordinals.com:8000/list/${outpoint}`)
-    .then((res) => res.json())
-    .then((data) => data)
-    .catch((error) => console.log(error));
-};
+    .then((res) => {
+      document.getElementById('result').innerHTML = res
+        .json()
+        .map((range) => `[${range[0]}, ${range[1]})<br>`)
+        .join('');
+    })
+    .catch((error) => (document.getElementById('error').innerHTML = error));
+}
 
-document.getElementById('list-form').addEventListener('submit', (e) => {
+document.getElementById('form').addEventListener('submit', (e) => {
   e.preventDefault();
-  list(document.getElementById('outpoint').value).then((data) => {
-    document.getElementById('list-result').innerHTML = data
-      .map((range) => `[${range[0]}, ${range[1]})<br>`)
-      .join('');
-  });
+  list(document.getElementById('outpoint').value);
 });
