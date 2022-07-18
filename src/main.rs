5f6288e148931f4c45d0a259e20e2533f127834f
Casey Rodarmor <casey@rodarmor.com>
2022-07-18 13:32:36 -0700

    Switch back to redb

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ use {
   integer_sqrt::IntegerSquareRoot,
   lazy_static::lazy_static,
   qrcode_generator::QrCodeEcc,
+  redb_database::{Database, WriteTransaction},
   secp256k1::{rand, schnorr::Signature, KeyPair, Secp256k1, SecretKey, XOnlyPublicKey},
   serde::{Deserialize, Serialize},
   std::{
@@ -46,23 +47,14 @@ use {
   tower_http::cors::{Any, CorsLayer},
 };
 
-#[cfg(feature = "redb")]
-use redb_database::{Database, WriteTransaction};
-
-#[cfg(not(feature = "redb"))]
-use lmdb_database::{Database, WriteTransaction};
-
 mod arguments;
 mod bytes;
 mod epoch;
 mod height;
 mod index;
-#[cfg(not(feature = "redb"))]
-mod lmdb_database;
 mod nft;
 mod options;
 mod ordinal;
-#[cfg(feature = "redb")]
 mod redb_database;
 mod sat_point;
 mod subcommand;
