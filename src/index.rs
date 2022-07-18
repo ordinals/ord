35da95efc2ea059899c502e7b23632c37801c854
Casey Rodarmor <casey@rodarmor.com>
2022-07-18 14:00:32 -0700

    Add back commit function and move writetx into own file

--- src/index.rs
+++ src/index.rs
@@ -2,68 +2,14 @@ use {
   super::*,
   bitcoincore_rpc::{Auth, Client, RpcApi},
   rayon::iter::{IntoParallelRefIterator, ParallelIterator},
+  write_transaction::WriteTransaction,
 };
+
+mod write_transaction;
 
 const HEIGHT_TO_HASH: TableDefinition<u64, [u8]> = TableDefinition::new("HEIGHT_TO_HASH");
 const OUTPOINT_TO_ORDINAL_RANGES: TableDefinition<[u8], [u8]> =
   TableDefinition::new("OUTPOINT_TO_ORDINAL_RANGES");
-
-pub(crate) struct WriteTransaction<'a> {
-  inner: DatabaseTransaction<'a>,
-  height_to_hash: Table<'a, u64, [u8]>,
-  outpoint_to_ordinal_ranges: Table<'a, [u8], [u8]>,
-}
-
-impl<'a> WriteTransaction<'a> {
-  pub(crate) fn new(database: &'a redb::Database) -> Result<Self> {
-    let inner = database.begin_write()?;
-    let height_to_hash = inner.open_table(&HEIGHT_TO_HASH)?;
-    let outpoint_to_ordinal_ranges = inner.open_table(&OUTPOINT_TO_ORDINAL_RANGES)?;
-
-    Ok(Self {
-      inner,
-      height_to_hash,
-      outpoint_to_ordinal_ranges,
-    })
-  }
-
-  pub(crate) fn height(&self) -> Result<u64> {
-    Ok(
-      self
-        .height_to_hash
-        .range(0..)?
-        .rev()
-        .next()
-        .map(|(height, _hash)| height + 1)
-        .unwrap_or(0),
-    )
-  }
-
-  pub(crate) fn blockhash_at_height(&self, height: u64) -> Result<Option<&[u8]>> {
-    Ok(self.height_to_hash.get(&height)?)
-  }
-
-  pub(crate) fn set_blockhash_at_height(&mut self, height: u64, blockhash: BlockHash) -> Result {
-    self.height_to_hash.insert(&height, &blockhash)?;
-    Ok(())
-  }
-
-  pub(crate) fn insert_outpoint(&mut self, outpoint: &[u8], ordinal_ranges: &[u8]) -> Result {
-    self
-      .outpoint_to_ordinal_ranges
-      .insert(outpoint, ordinal_ranges)?;
-    Ok(())
-  }
-
-  pub(crate) fn remove_outpoint(&mut self, outpoint: &[u8]) -> Result {
-    self.outpoint_to_ordinal_ranges.remove(outpoint)?;
-    Ok(())
-  }
-
-  pub(crate) fn get_ordinal_ranges(&self, outpoint: &[u8]) -> Result<Option<&[u8]>> {
-    Ok(self.outpoint_to_ordinal_ranges.get(outpoint)?)
-  }
-}
 
 pub(crate) struct Index {
   client: Client,
@@ -169,7 +115,7 @@ impl Index {
       let block = match self.block(height)? {
         Some(block) => block,
         None => {
-          wtx.inner.commit()?;
+          wtx.commit()?;
           return Ok(());
         }
       };
@@ -251,7 +197,7 @@ impl Index {
 
       wtx.set_blockhash_at_height(height, block.block_hash())?;
       if height % 1000 == 0 {
-        wtx.inner.commit()?;
+        wtx.commit()?;
         wtx = WriteTransaction::new(&self.database)?;
       }
 
@@ -261,7 +207,7 @@ impl Index {
       );
 
       if INTERRUPTS.load(atomic::Ordering::Relaxed) > 0 {
-        wtx.inner.commit()?;
+        wtx.commit()?;
         return Ok(());
       }
     }
---
+++ src/index/write_transaction.rs
@@ -0,0 +1,63 @@
+use super::*;
+
+pub(crate) struct WriteTransaction<'a> {
+  inner: DatabaseTransaction<'a>,
+  height_to_hash: Table<'a, u64, [u8]>,
+  outpoint_to_ordinal_ranges: Table<'a, [u8], [u8]>,
+}
+
+impl<'a> WriteTransaction<'a> {
+  pub(crate) fn new(database: &'a redb::Database) -> Result<Self> {
+    let inner = database.begin_write()?;
+    let height_to_hash = inner.open_table(&HEIGHT_TO_HASH)?;
+    let outpoint_to_ordinal_ranges = inner.open_table(&OUTPOINT_TO_ORDINAL_RANGES)?;
+
+    Ok(Self {
+      inner,
+      height_to_hash,
+      outpoint_to_ordinal_ranges,
+    })
+  }
+
+  pub(crate) fn height(&self) -> Result<u64> {
+    Ok(
+      self
+        .height_to_hash
+        .range(0..)?
+        .rev()
+        .next()
+        .map(|(height, _hash)| height + 1)
+        .unwrap_or(0),
+    )
+  }
+
+  pub(crate) fn blockhash_at_height(&self, height: u64) -> Result<Option<&[u8]>> {
+    Ok(self.height_to_hash.get(&height)?)
+  }
+
+  pub(crate) fn set_blockhash_at_height(&mut self, height: u64, blockhash: BlockHash) -> Result {
+    self.height_to_hash.insert(&height, &blockhash)?;
+    Ok(())
+  }
+
+  pub(crate) fn insert_outpoint(&mut self, outpoint: &[u8], ordinal_ranges: &[u8]) -> Result {
+    self
+      .outpoint_to_ordinal_ranges
+      .insert(outpoint, ordinal_ranges)?;
+    Ok(())
+  }
+
+  pub(crate) fn remove_outpoint(&mut self, outpoint: &[u8]) -> Result {
+    self.outpoint_to_ordinal_ranges.remove(outpoint)?;
+    Ok(())
+  }
+
+  pub(crate) fn get_ordinal_ranges(&self, outpoint: &[u8]) -> Result<Option<&[u8]>> {
+    Ok(self.outpoint_to_ordinal_ranges.get(outpoint)?)
+  }
+
+  pub(crate) fn commit(self) -> Result {
+    self.inner.commit()?;
+    Ok(())
+  }
+}
