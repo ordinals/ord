1c895955c056ccc4c11481f22c442dc50c67fe3b
Liam <liam@scalzulli.com>
2022-07-18 18:01:40 -0400

    Check different path for linux

--- tests/wallet.rs
+++ tests/wallet.rs
@@ -1,6 +1,7 @@
 use super::*;
 
 #[test]
+#[cfg(target_os = "macos")]
 fn init() -> Result {
   Test::new()?
     .command("wallet init")
@@ -8,6 +9,18 @@ fn init() -> Result {
     .expected_status(0)
     .expected_stderr("Wallet initialized.\n")
     .expected_path("Library/Application Support/ord/wallet.sqlite")
+    .run()
+}
+
+#[test]
+#[cfg(target_os = "linux")]
+fn init() -> Result {
+  Test::new()?
+    .command("wallet init")
+    .set_home_to_tempdir()
+    .expected_status(0)
+    .expected_stderr("Wallet initialized.\n")
+    .expected_path(".local/share/ord/wallet.sqlite")
     .run()
 }
 
