e92e8866b403be994f0922f47ec3d6baf779f867
Liam <liam@scalzulli.com>
2022-07-18 17:55:55 -0400

    Cleaner

--- tests/wallet.rs
+++ tests/wallet.rs
@@ -2,9 +2,7 @@ use super::*;
 
 #[test]
 fn init() -> Result {
-  let test = Test::new()?;
-
-  test
+  Test::new()?
     .command("wallet init")
     .set_home_to_tempdir()
     .expected_status(0)
@@ -15,9 +13,7 @@ fn init() -> Result {
 
 #[test]
 fn fund() -> Result {
-  let test = Test::new()?;
-
-  test
+  Test::new()?
     .command("wallet fund")
     .set_home_to_tempdir()
     .expected_stdout("tb1qtprrd8eadw3kd4h44yplkh85mj3hv0qwunj99h\n")
