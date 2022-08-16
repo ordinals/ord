2e026c88d7b951972a74cc6af953645ffbfd2106
Liam <liam@scalzulli.com>
2022-08-16 14:19:11 -0400

    Purse refactors

--- src/purse.rs
+++ src/purse.rs
@@ -1,9 +1,13 @@
 use super::*;
 
+use bdk::keys::DerivableKey;
+use bdk::miniscript::miniscript::Segwitv0;
+
 #[derive(Debug)]
 pub(crate) struct Purse {
-  pub(crate) wallet: bdk::wallet::Wallet<SqliteDatabase>,
   pub(crate) blockchain: RpcBlockchain,
+  pub(crate) options: Options,
+  pub(crate) wallet: bdk::wallet::Wallet<SqliteDatabase>,
 }
 
 impl Purse {
@@ -36,20 +40,7 @@ impl Purse {
     )?;
 
     wallet.sync(
-      &RpcBlockchain::from_config(&RpcConfig {
-        url: options.rpc_url(),
-        auth: Auth::Cookie {
-          file: options.cookie_file()?,
-        },
-        network: options.network,
-        wallet_name: wallet_name_from_descriptor(
-          Bip84((seed, None), KeychainKind::External),
-          None,
-          options.network,
-          &Secp256k1::new(),
-        )?,
-        skip_blocks: None,
-      })?,
+      &Self::blockchain(options, (seed, None))?,
       SyncOptions::default(),
     )?;
 
@@ -58,7 +49,7 @@ impl Purse {
     Ok(())
   }
 
-  pub(crate) fn load(options: &Options) -> Result<Self> {
+  pub(crate) fn load(options: Options) -> Result<Self> {
     let path = data_dir()
       .ok_or_else(|| anyhow!("Failed to retrieve data dir"))?
       .join("ord");
@@ -85,28 +76,19 @@ impl Purse {
       ),
     )?;
 
-    let blockchain = RpcBlockchain::from_config(&RpcConfig {
-      url: options.rpc_url(),
-      auth: Auth::Cookie {
-        file: options.cookie_file()?,
-      },
-      network: options.network,
-      wallet_name: wallet_name_from_descriptor(
-        Bip84(key, KeychainKind::External),
-        None,
-        options.network,
-        &Secp256k1::new(),
-      )?,
-      skip_blocks: None,
-    })?;
+    let blockchain = Self::blockchain(&options, key)?;
 
     wallet.sync(&blockchain, SyncOptions::default())?;
 
-    Ok(Self { wallet, blockchain })
+    Ok(Self {
+      blockchain,
+      options,
+      wallet,
+    })
   }
 
-  pub(crate) fn find(&self, options: &Options, ordinal: Ordinal) -> Result<LocalUtxo> {
-    let index = Index::index(options)?;
+  pub(crate) fn find(&self, ordinal: Ordinal) -> Result<LocalUtxo> {
+    let index = Index::index(&self.options)?;
 
     for utxo in self.wallet.list_unspent()? {
       if let Some(ranges) = index.list(utxo.outpoint)? {
@@ -119,5 +101,25 @@ impl Purse {
     }
 
     bail!("No utxo contains {}˚.", ordinal);
+  }
+
+  fn blockchain(
+    options: &Options,
+    key: impl DerivableKey<Segwitv0> + Clone,
+  ) -> Result<RpcBlockchain> {
+    Ok(RpcBlockchain::from_config(&RpcConfig {
+      url: options.rpc_url(),
+      auth: Auth::Cookie {
+        file: options.cookie_file()?,
+      },
+      network: options.network,
+      wallet_name: wallet_name_from_descriptor(
+        Bip84(key, KeychainKind::External),
+        None,
+        options.network,
+        &Secp256k1::new(),
+      )?,
+      skip_blocks: None,
+    })?)
   }
 }
--- src/subcommand/wallet/utxos.rs
+++ src/subcommand/wallet/utxos.rs
@@ -1,7 +1,7 @@
 use super::*;
 
 pub(crate) fn run(options: Options) -> Result {
-  for utxo in Purse::load(&options)?.wallet.list_unspent()? {
+  for utxo in Purse::load(options)?.wallet.list_unspent()? {
     println!(
       "{}:{} {}",
       utxo.outpoint.txid, utxo.outpoint.vout, utxo.txout.value
