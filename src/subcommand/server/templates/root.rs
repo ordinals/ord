a0acf9103630621b841c799373361284fa7784a7
Casey Rodarmor <casey@rodarmor.com>
2022-08-17 15:16:49 -0700

    fix-tests

--- src/subcommand/server/templates/root.rs
+++ src/subcommand/server/templates/root.rs
@@ -38,7 +38,14 @@ mod tests {
       }
       .to_string(),
       "
-        <h1>Recent Blocks</h1>
+        <h1>Ordinals</h1>
+        <nav>
+          <a href=https://rodarmor.com/blog/ordinal-theory/>Theory</a>
+          <a href=https://github.com/casey/ord/blob/master/bip.mediawiki>BIP</a>
+          <a href=https://github.com/casey/ord>GitHub</a>
+          <a href=https://discord.gg/87cjuz4FYg>Discord</a>
+        </nav>
+        <h2>Recent Blocks</h2>
         <ul>
           <li>1 - <a href=/block/1111111111111111111111111111111111111111111111111111111111111111>1111111111111111111111111111111111111111111111111111111111111111</a></li>
           <li>0 - <a href=/block/0000000000000000000000000000000000000000000000000000000000000000>0000000000000000000000000000000000000000000000000000000000000000</a></li>
