2b892ba08eabf7276ca8f069c804b67208880eb7
Liam <liam@scalzulli.com>
2022-08-17 16:38:00 -0400

    Remove space

--- src/subcommand/server/templates.rs
+++ src/subcommand/server/templates.rs
@@ -50,7 +50,7 @@ mod tests {
 <html lang=en>
   <head>
     <meta charset=utf-8>
-    <meta name=viewport content='width=device-width, initial-scale=1.0'>
+    <meta name=viewport content='width=device-width,initial-scale=1.0'>
     <title>Foo</title>
   </head>
   <body>
