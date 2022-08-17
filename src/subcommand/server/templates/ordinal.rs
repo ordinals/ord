f45f43957d6c49fdbf9071ee90b4f553b98d1257
liam <31192478+terror@users.noreply.github.com>
2022-08-17 15:35:26 -0400

    Add next and prev links to /ordinal (#329)

--- src/subcommand/server/templates/ordinal.rs
+++ src/subcommand/server/templates/ordinal.rs
@@ -44,6 +44,66 @@ mod tests {
           <dt>rarity</dt><dd>mythic</dd>
           <dt>block time</dt><dd>1970-01-01 00:00:00</dd>
         </dl>
+        <a>prev</a>
+        <a href=/ordinal/1>next</a>
+      "
+      .unindent()
+    );
+  }
+
+  #[test]
+  fn ordinal_previous() {
+    assert_eq!(
+      OrdinalHtml {
+        ordinal: Ordinal::LAST,
+        blocktime: Blocktime::Confirmed(0),
+      }
+      .to_string(),
+      "
+        <h1>Ordinal 2099999997689999</h1>
+        <dl>
+          <dt>decimal</dt><dd>6929999.0</dd>
+          <dt>degree</dt><dd>5°209999′1007″0‴</dd>
+          <dt>name</dt><dd>a</dd>
+          <dt>height</dt><dd>6929999</dd>
+          <dt>cycle</dt><dd>5</dd>
+          <dt>epoch</dt><dd>32</dd>
+          <dt>period</dt><dd>3437</dd>
+          <dt>offset</dt><dd>0</dd>
+          <dt>rarity</dt><dd>uncommon</dd>
+          <dt>block time</dt><dd>1970-01-01 00:00:00</dd>
+        </dl>
+        <a href=/ordinal/2099999997689998>prev</a>
+        <a>next</a>
+      "
+      .unindent()
+    );
+  }
+
+  #[test]
+  fn ordinal_next_and_previous() {
+    assert_eq!(
+      OrdinalHtml {
+        ordinal: Ordinal(1),
+        blocktime: Blocktime::Confirmed(0),
+      }
+      .to_string(),
+      "
+        <h1>Ordinal 1</h1>
+        <dl>
+          <dt>decimal</dt><dd>0.1</dd>
+          <dt>degree</dt><dd>0°0′0″1‴</dd>
+          <dt>name</dt><dd>nvtdijuwxlo</dd>
+          <dt>height</dt><dd>0</dd>
+          <dt>cycle</dt><dd>0</dd>
+          <dt>epoch</dt><dd>0</dd>
+          <dt>period</dt><dd>0</dd>
+          <dt>offset</dt><dd>1</dd>
+          <dt>rarity</dt><dd>common</dd>
+          <dt>block time</dt><dd>1970-01-01 00:00:00</dd>
+        </dl>
+        <a href=/ordinal/0>prev</a>
+        <a href=/ordinal/2>next</a>
       "
       .unindent()
     );
