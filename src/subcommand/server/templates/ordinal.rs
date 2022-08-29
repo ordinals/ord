6fa0dfe0656b17b2a086b023f470d2a72b1480dd
Casey Rodarmor <casey@rodarmor.com>
2022-08-29 12:01:31 -0700

    Improve names for a couple of properties (#377)

--- src/subcommand/server/templates/ordinal.rs
+++ src/subcommand/server/templates/ordinal.rs
@@ -30,13 +30,13 @@ mod tests {
           <dt>decimal</dt><dd>0.0</dd>
           <dt>degree</dt><dd>0°0′0″0‴</dd>
           <dt>name</dt><dd>nvtdijuwxlp</dd>
-          <dt>height</dt><dd>0</dd>
+          <dt>block</dt><dd>0</dd>
           <dt>cycle</dt><dd>0</dd>
           <dt>epoch</dt><dd>0</dd>
           <dt>period</dt><dd>0</dd>
           <dt>offset</dt><dd>0</dd>
           <dt>rarity</dt><dd><span class=mythic>mythic</span></dd>
-          <dt>block time</dt><dd>1970-01-01 00:00:00</dd>
+          <dt>time</dt><dd>1970-01-01 00:00:00</dd>
         </dl>
         <a>prev</a>
         <a href=/ordinal/1>next</a>
@@ -59,13 +59,13 @@ mod tests {
           <dt>decimal</dt><dd>6929999.0</dd>
           <dt>degree</dt><dd>5°209999′1007″0‴</dd>
           <dt>name</dt><dd>a</dd>
-          <dt>height</dt><dd>6929999</dd>
+          <dt>block</dt><dd>6929999</dd>
           <dt>cycle</dt><dd>5</dd>
           <dt>epoch</dt><dd>32</dd>
           <dt>period</dt><dd>3437</dd>
           <dt>offset</dt><dd>0</dd>
           <dt>rarity</dt><dd><span class=uncommon>uncommon</span></dd>
-          <dt>block time</dt><dd>1970-01-01 00:00:00</dd>
+          <dt>time</dt><dd>1970-01-01 00:00:00</dd>
         </dl>
         <a href=/ordinal/2099999997689998>prev</a>
         <a>next</a>
@@ -88,13 +88,13 @@ mod tests {
           <dt>decimal</dt><dd>0.1</dd>
           <dt>degree</dt><dd>0°0′0″1‴</dd>
           <dt>name</dt><dd>nvtdijuwxlo</dd>
-          <dt>height</dt><dd>0</dd>
+          <dt>block</dt><dd>0</dd>
           <dt>cycle</dt><dd>0</dd>
           <dt>epoch</dt><dd>0</dd>
           <dt>period</dt><dd>0</dd>
           <dt>offset</dt><dd>1</dd>
           <dt>rarity</dt><dd><span class=common>common</span></dd>
-          <dt>block time</dt><dd>1970-01-01 00:00:00</dd>
+          <dt>time</dt><dd>1970-01-01 00:00:00</dd>
         </dl>
         <a href=/ordinal/0>prev</a>
         <a href=/ordinal/2>next</a>
