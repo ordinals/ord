0ed3de8b3d8457a68ce96c8c1a442f872dd6ae68
jurraca <jurraca@users.noreply.github.com>
2022-09-04 15:09:01 -0500

    Add shell.nix (#475)

---
+++ shell.nix
@@ -0,0 +1,12 @@
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
+  with nixpkgs;
+  stdenv.mkDerivation {
+    name = "ord-shell";
+    buildInputs = [
+      nixpkgs.latest.rustChannels.stable.rust
+      just
+    ];
+  }
