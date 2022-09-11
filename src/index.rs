618d26a6fe9e2726d50d1afd67564fdfecaf671b
Casey Rodarmor <casey@rodarmor.com>
2022-09-11 13:00:23 -0700

    Update dependencies

--- src/index.rs
+++ src/index.rs
@@ -44,7 +44,7 @@ impl Index {
       Ok(database) => database,
       Err(redb::Error::Io(error)) if error.kind() == io::ErrorKind::NotFound => unsafe {
         Database::builder()
-          .set_write_strategy(WriteStrategy::Throughput)
+          .set_write_strategy(WriteStrategy::TwoPhase)
           .create(&database_path, options.max_index_size().0)?
       },
       Err(error) => return Err(error.into()),
