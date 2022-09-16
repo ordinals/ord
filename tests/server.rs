4b84c8e2653f17194610a74a7bfc46ef35e19ea4
Casey Rodarmor <casey@rodarmor.com>
2022-09-16 18:35:28 -0500

    Remove old tests

--- tests/server.rs
+++ tests/server.rs
@@ -321,26 +321,6 @@ fn static_asset() {
 }
 
 #[test]
-fn faq() {
-  let mut state = State::new();
-  state.request_regex(
-    "faq",
-    200,
-    r".*<title>Ordinal FAQ</title>.*<h1>Ordinal FAQ</h1>.*",
-  );
-}
-
-#[test]
-fn bounties() {
-  let mut state = State::new();
-  state.request_regex(
-    "bounties",
-    200,
-    r".*<title>Ordinal Bounties</title>.*<h1>Ordinal Bounties</h1>.*",
-  );
-}
-
-#[test]
 fn favicon() {
   let mut state = State::new();
   state.request_expected("favicon.ico", 200, Expected::Ignore);
