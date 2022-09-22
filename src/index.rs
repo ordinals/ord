7205ed8bd3b243ed9168d5a335ae91a9d84492ee
Casey Rodarmor <casey@rodarmor.com>
2022-09-22 12:47:01 -0700

    Use Checksum to make tests strategy

--- src/index.rs
+++ src/index.rs
@@ -64,7 +64,7 @@ impl Index {
       Ok(database) => database,
       Err(redb::Error::Io(error)) if error.kind() == io::ErrorKind::NotFound => unsafe {
         Database::builder()
-          .set_write_strategy(WriteStrategy::TwoPhase)
+          .set_write_strategy(WriteStrategy::Checksum)
           .create(&database_path, options.max_index_size().0)?
       },
       Err(error) => return Err(error.into()),
