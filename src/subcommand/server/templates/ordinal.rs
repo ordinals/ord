f823efdafab1a2d7e0eb66894ba35402bb77adec
Casey Rodarmor <casey@rodarmor.com>
2022-09-26 12:48:38 -0700

    Add prime trait

--- src/subcommand/server/templates/ordinal.rs
+++ src/subcommand/server/templates/ordinal.rs
@@ -36,6 +36,7 @@ mod tests {
           <dt>period</dt><dd>0</dd>
           <dt>offset</dt><dd>0</dd>
           <dt>rarity</dt><dd><span class=mythic>mythic</span></dd>
+          <dt>prime</dt><dd>false</dd>
           <dt>time</dt><dd>1970-01-01 00:00:00</dd>
           <dt>percentile</dt><dd>0%</dd>
         </dl>
@@ -47,32 +48,14 @@ mod tests {
   }
 
   #[test]
-  fn ordinal_previous() {
-    assert_eq!(
+  fn two_is_prime() {
+    assert_regex_match!(
       OrdinalHtml {
-        ordinal: Ordinal::LAST,
+        ordinal: Ordinal(2),
         blocktime: Blocktime::Confirmed(0),
       }
       .to_string(),
-      "
-        <h1>Ordinal 2099999997689999</h1>
-        <dl>
-          <dt>decimal</dt><dd>6929999.0</dd>
-          <dt>degree</dt><dd>5°209999′1007″0‴</dd>
-          <dt>name</dt><dd>a</dd>
-          <dt>block</dt><dd>6929999</dd>
-          <dt>cycle</dt><dd>5</dd>
-          <dt>epoch</dt><dd>32</dd>
-          <dt>period</dt><dd>3437</dd>
-          <dt>offset</dt><dd>0</dd>
-          <dt>rarity</dt><dd><span class=uncommon>uncommon</span></dd>
-          <dt>time</dt><dd>1970-01-01 00:00:00</dd>
-          <dt>percentile</dt><dd>100%</dd>
-        </dl>
-        <a href=/ordinal/2099999997689998>prev</a>
-        <a>next</a>
-      "
-      .unindent()
+      ".*<dt>prime</dt><dd>true</dd>.*",
     );
   }
 
@@ -96,6 +79,7 @@ mod tests {
           <dt>period</dt><dd>0</dd>
           <dt>offset</dt><dd>1</dd>
           <dt>rarity</dt><dd><span class=common>common</span></dd>
+          <dt>prime</dt><dd>false</dd>
           <dt>time</dt><dd>1970-01-01 00:00:00</dd>
           <dt>percentile</dt><dd>0.000000000000047619047671428595%</dd>
         </dl>
