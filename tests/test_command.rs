5f42e84dd629b7c8f0a563927347e47afe3f9b9b
Casey Rodarmor <casey@rodarmor.com>
2022-09-27 14:00:55 -0700

    Get test working

--- tests/test_command.rs
+++ tests/test_command.rs
@@ -5,6 +5,7 @@ pub(crate) struct TestCommand {
   expected_status: i32,
   expected_stderr: Expected,
   expected_stdout: Expected,
+  rpc_server_url: Option<String>,
   tempdir: TempDir,
 }
 
@@ -15,7 +16,15 @@ impl TestCommand {
       expected_status: 0,
       expected_stderr: Expected::Ignore,
       expected_stdout: Expected::String(String::new()),
+      rpc_server_url: None,
       args,
+    }
+  }
+
+  pub(crate) fn rpc_server(self, rpc_server: &test_bitcoincore_rpc::Handle) -> Self {
+    Self {
+      rpc_server_url: Some(rpc_server.url()),
+      ..self
     }
   }
 
@@ -48,7 +57,20 @@ impl TestCommand {
   }
 
   pub(crate) fn run(self) {
-    let output = Command::new(executable_path("ord"))
+    let mut command = Command::new(executable_path("ord"));
+
+    if let Some(rpc_server_url) = self.rpc_server_url {
+      let cookiefile = self.tempdir.path().join("cookie");
+      fs::write(&cookiefile, "username:password").unwrap();
+      command.args(&[
+        "--rpc-url",
+        &rpc_server_url,
+        "--cookie-file",
+        cookiefile.to_str().unwrap(),
+      ]);
+    }
+
+    let output = command
       .stdin(Stdio::null())
       .stdout(Stdio::piped())
       .stderr(if !matches!(self.expected_stderr, Expected::Ignore) {
@@ -56,6 +78,7 @@ impl TestCommand {
       } else {
         Stdio::inherit()
       })
+      .env("HOME", self.tempdir.path())
       .current_dir(&self.tempdir)
       .args(self.args.split_whitespace())
       .output()
