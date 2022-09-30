1f43cae232a45175c866026da76a460f7871bf38
raphjaph <raphjaph@protonmail.com>
2022-09-30 22:15:44 +0200

    remove list command

--- src/subcommand/wallet.rs
+++ src/subcommand/wallet.rs
@@ -8,14 +8,12 @@ mod identify;
 #[derive(Debug, Parser)]
 pub(crate) enum Wallet {
   Identify,
-  List,
 }
 
 impl Wallet {
   pub(crate) fn run(self, options: Options) -> Result<()> {
     match self {
       Self::Identify => identify::run(options),
-      Self::List => Ok(()),
     }
   }
 }
