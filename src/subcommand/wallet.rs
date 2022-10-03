d0c3cd0db41825c1feb6b2431e87f31bab8a6d1a
raphjaph <raphjaph@protonmail.com>
2022-10-04 01:02:17 +0200

    .map() for the win

--- src/subcommand/wallet.rs
+++ src/subcommand/wallet.rs
@@ -3,6 +3,26 @@ use {super::*, bitcoincore_rpc::RpcApi};
 mod identify;
 mod list;
 
+fn list_unspent(options: Options) -> Result<Vec<(OutPoint, Vec<(u64, u64)>)>> {
+  let index = Index::open(&options)?;
+  index.index()?;
+
+  let client = options.bitcoin_rpc_client()?;
+
+  client
+    .list_unspent(None, None, None, None, None)?
+    .iter()
+    .map(|utxo| {
+      let outpoint = OutPoint::new(utxo.txid, utxo.vout);
+      match index.list(outpoint)? {
+        Some(List::Unspent(ordinal_ranges)) => Ok((outpoint, ordinal_ranges)),
+        Some(List::Spent) => bail!("Output {outpoint} in wallet but is spent according to index"),
+        None => bail!("Ordinals index has not seen {outpoint}"),
+      }
+    })
+    .collect()
+}
+
 #[derive(Debug, Parser)]
 pub(crate) enum Wallet {
   Identify,
@@ -16,25 +36,6 @@ impl Wallet {
       Self::List => list::run(options),
     }
   }
-}
-
-fn list_unspent(options: Options) -> Result<Vec<(OutPoint, Vec<(u64, u64)>)>> {
-  let index = Index::open(&options)?;
-  index.index()?;
-
-  let client = options.bitcoin_rpc_client()?;
-
-  let mut utxos = Vec::new();
-  for utxo in client.list_unspent(None, None, None, None, None)? {
-    let outpoint = OutPoint::new(utxo.txid, utxo.vout);
-    match index.list(outpoint)? {
-      Some(List::Unspent(ordinal_ranges)) => utxos.push((outpoint, ordinal_ranges)),
-      Some(List::Spent) => bail!("Output {outpoint} in wallet but is spent according to index"),
-      None => bail!("Ordinals index has not seen {outpoint}"),
-    }
-  }
-
-  Ok(utxos)
 }
 
 #[cfg(test)]
