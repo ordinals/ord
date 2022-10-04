f4f344c62a93bee78e3fc6932daca5499c6fb808
Casey Rodarmor <casey@rodarmor.com>
2022-10-03 20:10:01 -0700

    Move percentile

--- src/subcommand/server/templates/ordinal.rs
+++ src/subcommand/server/templates/ordinal.rs
@@ -29,6 +29,7 @@ mod tests {
         <dl>
           <dt>decimal</dt><dd>0.0</dd>
           <dt>degree</dt><dd>0°0′0″0‴</dd>
+          <dt>percentile</dt><dd>0%</dd>
           <dt>name</dt><dd>nvtdijuwxlp</dd>
           <dt>cycle</dt><dd>0</dd>
           <dt>epoch</dt><dd>0</dd>
@@ -38,7 +39,6 @@ mod tests {
           <dt>rarity</dt><dd><span class=mythic>mythic</span></dd>
           <dt>prime</dt><dd>false</dd>
           <dt>time</dt><dd>1970-01-01 00:00:00</dd>
-          <dt>percentile</dt><dd>0%</dd>
         </dl>
         <a>prev</a>
         <a href=/ordinal/1>next</a>
@@ -72,6 +72,7 @@ mod tests {
         <dl>
           <dt>decimal</dt><dd>0.1</dd>
           <dt>degree</dt><dd>0°0′0″1‴</dd>
+          <dt>percentile</dt><dd>0.000000000000047619047671428595%</dd>
           <dt>name</dt><dd>nvtdijuwxlo</dd>
           <dt>cycle</dt><dd>0</dd>
           <dt>epoch</dt><dd>0</dd>
@@ -81,7 +82,6 @@ mod tests {
           <dt>rarity</dt><dd><span class=common>common</span></dd>
           <dt>prime</dt><dd>false</dd>
           <dt>time</dt><dd>1970-01-01 00:00:00</dd>
-          <dt>percentile</dt><dd>0.000000000000047619047671428595%</dd>
         </dl>
         <a href=/ordinal/0>prev</a>
         <a href=/ordinal/2>next</a>
