c1b0dfe6555d74b4bc67fa6017f651faf1c65626
raphjaph <raphjaph@protonmail.com>
2022-11-30 23:15:39 +0100

    test fails

--- tests/server.rs
+++ tests/server.rs
@@ -77,3 +77,57 @@ HELLOWORLD.*",
     ),
   )
 }
+
+#[test]
+fn inscription_page_after_send() {
+  let rpc_server = test_bitcoincore_rpc::spawn_with(Network::Regtest, "ord");
+  let txid = rpc_server.mine_blocks(1)[0].txdata[0].txid();
+
+  let stdout = CommandBuilder::new(format!(
+    "--chain regtest wallet inscribe --satpoint {txid}:0:0 --file hello.txt"
+  ))
+  .write("hello.txt", "HELLOWORLD")
+  .rpc_server(&rpc_server)
+  .stdout_regex("commit\t[[:xdigit:]]{64}\nreveal\t[[:xdigit:]]{64}\n")
+  .run();
+
+  let reveal_tx = stdout.split("reveal\t").collect::<Vec<&str>>()[1].trim();
+
+  rpc_server.mine_blocks(1);
+
+  let ord_server = TestServer::spawn_with_args(&rpc_server, &[]);
+  ord_server.assert_response_regex(
+    &format!("/inscription/{}", reveal_tx),
+    &format!(
+      ".*<h1>Inscription</h1>
+<dl>
+  <dt>satpoint</dt>
+  <dd>{reveal_tx}:0:0</dd>
+</dl>
+HELLOWORLD.*",
+    ),
+  );
+
+  let txid = CommandBuilder::new(format!(
+    "--chain regtest wallet send {txid}:0:0 bcrt1q6rhpng9evdsfnn833a4f4vej0asu6dk5srld6x"
+  ))
+  .write("hello.txt", "HELLOWORLD")
+  .rpc_server(&rpc_server)
+  .stdout_regex(".*")
+  .run();
+
+  rpc_server.mine_blocks(1);
+
+  let ord_server = TestServer::spawn_with_args(&rpc_server, &[]);
+  ord_server.assert_response_regex(
+    &format!("/inscription/{}", reveal_tx),
+    &format!(
+      ".*<h1>Inscription</h1>
+<dl>
+  <dt>satpoint</dt>
+  <dd>{txid}:0:0</dd>
+</dl>
+HELLOWORLD.*",
+    ),
+  )
+}
