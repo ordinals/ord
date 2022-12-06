90c90c300acf14b3d02b91b4a91bd46210639b96
Casey Rodarmor <casey@rodarmor.com>
2022-12-06 14:18:27 -0800

    Limit inscription content to 1024 bytes on signet and testnet (#896)

--- src/subcommand/wallet/inscribe.rs
+++ src/subcommand/wallet/inscribe.rs
@@ -24,7 +24,7 @@ impl Inscribe {
   pub(crate) fn run(self, options: Options) -> Result {
     let client = options.bitcoin_rpc_client_mainnet_forbidden("ord wallet inscribe")?;
 
-    let inscription = Inscription::from_file(self.file)?;
+    let inscription = Inscription::from_file(options.chain, self.file)?;
 
     let index = Index::open(&options)?;
     index.update()?;
