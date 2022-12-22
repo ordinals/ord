10ba50321ae849dc1fbe357fbb9177259628f22c
rot13maxi <rot13maxi@protonmail.com>
2022-12-21 22:32:33 -0500

    add option to transaction-list command to specify count

--- src/subcommand/wallet/transactions.rs
+++ src/subcommand/wallet/transactions.rs
@@ -1,13 +1,19 @@
 use super::*;
 
 #[derive(Debug, Parser)]
-pub(crate) struct Transactions {}
+pub(crate) struct Transactions {
+  #[clap(
+  long,
+  help = "Max number of transactions to fetch (defaults to 10)."
+  )]
+  count: Option<usize>,
+}
 
 impl Transactions {
   pub(crate) fn run(self, options: Options) -> Result {
     let txs = options
       .bitcoin_rpc_client()?
-      .list_transactions(None, None, None, None)?
+      .list_transactions(None, self.count, None, None)?
       .iter()
       .map(|tx| (tx.info.txid, tx.info.confirmations))
       .collect::<Vec<(Txid, i32)>>();
