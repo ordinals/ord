ee482e9d9a31b7c35dccbecadfe2d82939dd48e0
Casey Rodarmor <casey@rodarmor.com>
2023-01-02 12:20:41 -0800

    Rename `ord wallet utxos` → `ord wallet outputs`

--- tests/wallet.rs
+++ tests/wallet.rs
@@ -631,14 +631,14 @@ fn receive() {
 }
 
 #[test]
-fn utxos() {
+fn outputs() {
   let rpc_server = test_bitcoincore_rpc::spawn();
 
   let coinbase_tx = &rpc_server.mine_blocks_with_subsidy(1, 1_000_000)[0].txdata[0];
   let outpoint = OutPoint::new(coinbase_tx.txid(), 0);
   let amount = coinbase_tx.output[0].value;
 
-  CommandBuilder::new("wallet utxos")
+  CommandBuilder::new("wallet outputs")
     .rpc_server(&rpc_server)
     .expected_stdout(format!("{outpoint}\t{amount}\n"))
     .run();
