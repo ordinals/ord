d05c03a97b26269b2e53f64a1fd1a9e329a5cf77
Casey Rodarmor <casey@rodarmor.com>
2023-01-13 23:00:20 -0800

    Simplify

--- static/preview-text.js
+++ static/preview-text.js
@@ -1,30 +1,6 @@
-let pre = document.getElementsByTagName('pre')[0];
-
-// TODO:
-// - add margins
-// - make it work with noscript
-// - could use a fixed font with fixed dimensions, and make it pure css
-// - fix overflow cases
-
-let rect = pre.getBoundingClientRect();
-
-console.log('pre', rect);
-console.log('body', document.body.clientWidth, document.body.clientHeight);
-
-let last = null;
-
-if (false) {
-  pre.style.opacity = 1;
-} else {
-  function resize() {
-    let size = Math.min(document.body.clientWidth / rect.width, document.body.clientHeight / rect.height);
-    if(size != last) {
-      pre.style.fontSize = `${size}em`;
-      pre.style.opacity = 1;
-      last = size;
-    }
-  }
-
-  addEventListener("resize", resize);
-  resize();
-}
+let pre = document.querySelector('body > pre');
+let { width, height } = pre.getBoundingClientRect();
+let columns = width / 16;
+let rows = height / 16;
+pre.style.fontSize = `min(${100/columns}vw, ${100/rows}vh)`;
+pre.style.opacity = 1;
