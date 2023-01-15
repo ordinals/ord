30cfb58217c8936dc83a0b4e949f2eee5a469c29
Casey Rodarmor <casey@rodarmor.com>
2023-01-15 03:15:25 -0800

    Add a margin

--- static/preview-text.js
+++ static/preview-text.js
@@ -2,5 +2,5 @@ let pre = document.querySelector('body > pre');
 let { width, height } = pre.getBoundingClientRect();
 let columns = width / 16;
 let rows = height / 16;
-pre.style.fontSize = `min(${100/columns}vw, ${100/rows}vh)`;
+pre.style.fontSize = `min(${95/columns}vw, ${95/rows}vh)`;
 pre.style.opacity = 1;
