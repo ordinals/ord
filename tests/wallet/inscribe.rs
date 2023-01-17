9ee893e340241250b2b95d654d95d9f99a516526
Casey Rodarmor <casey@rodarmor.com>
2023-01-17 13:33:04 -0800

    Test huge inscriptions

--- tests/wallet/inscribe.rs
+++ tests/wallet/inscribe.rs
@@ -25,6 +25,20 @@ fn inscribe_creates_inscriptions() {
 }
 
 #[test]
+fn inscribe_works_with_huge_expensive_inscriptions() {
+  let rpc_server = test_bitcoincore_rpc::spawn();
+  create_wallet(&rpc_server);
+  let txid = rpc_server.mine_blocks(1)[0].txdata[0].txid();
+
+  CommandBuilder::new(format!(
+    "wallet inscribe foo.txt --satpoint {txid}:0:0 --fee-rate 10"
+  ))
+  .write("foo.txt", [0; 350_000])
+  .rpc_server(&rpc_server)
+  .output::<Inscribe>();
+}
+
+#[test]
 fn inscribe_fails_if_bitcoin_core_is_too_old() {
   let rpc_server = test_bitcoincore_rpc::builder().version(230000).build();
 
@@ -66,7 +80,7 @@ fn inscribe_unknown_file_extension() {
 }
 
 #[test]
-fn inscribe_exceeds_push_byte_limit() {
+fn inscribe_exceeds_chain_limit() {
   let rpc_server = test_bitcoincore_rpc::builder()
     .network(Network::Signet)
     .build();
