9e95666a7fdd9e03bd74a060c7b4c394685874ae
Casey Rodarmor <casey@rodarmor.com>
2023-01-18 12:37:02 -0800

    Fix tests

--- src/templates/inscriptions.rs
+++ src/templates/inscriptions.rs
@@ -22,6 +22,8 @@ mod tests {
     assert_regex_match!(
       InscriptionsHtml {
         inscriptions: vec![inscription_id(1), inscription_id(2)],
+        prev: None,
+        next: None,
       },
       "
         <h1>Inscriptions</h1>
@@ -29,6 +31,8 @@ mod tests {
           <a href=/inscription/1{64}i1><iframe .* src=/preview/1{64}i1></iframe></a>
           <a href=/inscription/2{64}i2><iframe .* src=/preview/2{64}i2></iframe></a>
         </div>
+        prev
+        next
       "
       .unindent()
     );
