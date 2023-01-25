23ed9cc966753e8c9a52494be9dc3b05539f1f0b
Casey Rodarmor <casey@rodarmor.com>
2023-01-25 11:04:26 -0800

    Add template tests

--- src/templates/inscriptions.rs
+++ src/templates/inscriptions.rs
@@ -18,7 +18,7 @@ mod tests {
   use super::*;
 
   #[test]
-  fn inscriptions() {
+  fn without_prev_and_next() {
     assert_regex_match!(
       InscriptionsHtml {
         inscriptions: vec![inscription_id(1), inscription_id(2)],
@@ -33,6 +33,27 @@ mod tests {
         </div>
         prev
         next
+      "
+      .unindent()
+    );
+  }
+
+  #[test]
+  fn with_prev_and_next() {
+    assert_regex_match!(
+      InscriptionsHtml {
+        inscriptions: vec![inscription_id(1), inscription_id(2)],
+        prev: Some(1),
+        next: Some(2),
+      },
+      "
+        <h1>Inscriptions</h1>
+        <div class=thumbnails>
+          <a href=/inscription/1{64}i1><iframe .* src=/preview/1{64}i1></iframe></a>
+          <a href=/inscription/2{64}i2><iframe .* src=/preview/2{64}i2></iframe></a>
+        </div>
+        <a class=prev href=/inscriptions/1>prev</a>
+        <a class=next href=/inscriptions/2>next</a>
       "
       .unindent()
     );
