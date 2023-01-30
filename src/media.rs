bfd34060067b8d02b47270e9a457842182f457a0
Hunter Trujillo <cryptoquick@pm.me>
2023-01-29 19:19:22 -0700

    Add mp4 to supported media types.

--- src/media.rs
+++ src/media.rs
@@ -63,6 +63,7 @@ const TABLE: &[(&str, Media, &[&str])] = &[
   ("text/html;charset=utf-8", Media::Iframe, &["html"]),
   ("text/plain;charset=utf-8", Media::Text, &["txt"]),
   ("video/webm", Media::Video, &["webm"]),
+  ("video/mp4", Media::Video, &["mp4"]),
 ];
 
 #[cfg(test)]
