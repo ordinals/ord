8348a12d10b5473e75d2211b8afa4ec227375db4
Casey Rodarmor <casey@rodarmor.com>
2023-02-01 08:19:23 -0800

    Render JSON and YAML inscriptions as text

--- src/media.rs
+++ src/media.rs
@@ -50,6 +50,8 @@ impl FromStr for Media {
 }
 
 const TABLE: &[(&str, Media, &[&str])] = &[
+  ("application/json", Media::Text, &["json"]),
+  ("application/yaml", Media::Text, &["yaml"]),
   ("application/pdf", Media::Pdf, &["pdf"]),
   ("application/pgp-signature", Media::Text, &["asc"]),
   ("audio/flac", Media::Audio, &["flac"]),
