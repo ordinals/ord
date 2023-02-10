2235674e5f3d527363ae8ef7911ba4b651e220aa
rot13maxi <rot13maxi@protonmail.com>
2023-02-09 21:27:24 -0500

    fmt

--- tests/wallet/inscribe.rs
+++ tests/wallet/inscribe.rs
@@ -362,18 +362,23 @@ fn inscribe_to_specific_destination() {
   rpc_server.mine_blocks(1);
 
   let destination = CommandBuilder::new("wallet receive")
-      .rpc_server(&rpc_server)
-      .output::<ord::subcommand::wallet::receive::Output>().address;
+    .rpc_server(&rpc_server)
+    .output::<ord::subcommand::wallet::receive::Output>()
+    .address;
 
-  let txid = CommandBuilder::new(format!("wallet inscribe --destination {destination} degenerate.png"))
-      .write("degenerate.png", [1; 520])
-      .rpc_server(&rpc_server)
-      .output::<Inscribe>()
-      .reveal;
+  let txid = CommandBuilder::new(format!(
+    "wallet inscribe --destination {destination} degenerate.png"
+  ))
+  .write("degenerate.png", [1; 520])
+  .rpc_server(&rpc_server)
+  .output::<Inscribe>()
+  .reveal;
   let reveal_tx = &rpc_server.mempool()[1]; // item 0 is the commit, item 1 is the reveal.
   assert_eq!(reveal_tx.txid(), txid);
-  assert_eq!(reveal_tx.output.first().unwrap().script_pubkey, destination.script_pubkey());
-
+  assert_eq!(
+    reveal_tx.output.first().unwrap().script_pubkey,
+    destination.script_pubkey()
+  );
 }
 #[test]
 fn inscribe_with_no_limit() {
