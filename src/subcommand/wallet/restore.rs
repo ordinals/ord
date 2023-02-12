ac377c27a7946ad2dea4e09ce7056dc81f566dca
Psifour <OutdatedConcept@gmail.com>
2023-02-11 23:44:10 -0600

    Linting

--- src/subcommand/wallet/restore.rs
+++ src/subcommand/wallet/restore.rs
@@ -4,17 +4,16 @@ use super::*;
 pub(crate) struct Restore {
   #[clap(help = "Restore wallet from <MNEMONIC>")]
   mnemonic: Mnemonic,
-  #[clap(
-  long,
-  short = 'p',
-  help = "Use <PASSPHRASE> when deriving wallet"
-  )]
+  #[clap(long, short = 'p', help = "Use <PASSPHRASE> when deriving wallet")]
   pub(crate) passphrase: Option<String>,
 }
 
 impl Restore {
   pub(crate) fn run(self, options: Options) -> Result {
-    initialize_wallet(&options, self.mnemonic.to_seed(self.passphrase.unwrap_or("".into())))?;
+    initialize_wallet(
+      &options,
+      self.mnemonic.to_seed(self.passphrase.unwrap_or("".into())),
+    )?;
 
     Ok(())
   }
