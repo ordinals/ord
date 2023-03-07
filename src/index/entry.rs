c781e555c976affb2e57c163951afebeffe98b36
raphjaph <raphjaph@protonmail.com>
2023-03-06 18:06:57 -0800

    stash

--- src/index/entry.rs
+++ src/index/entry.rs
@@ -87,79 +87,6 @@ impl Entry for InscriptionId {
   }
 }
 
-pub(super) type OutPointValue = [u8; 36];
-
-impl Entry for OutPoint {
-  type Value = OutPointValue;
-
-  fn load(value: Self::Value) -> Self {
-    Decodable::consensus_decode(&mut io::Cursor::new(value)).unwrap()
-  }
-
-  fn store(self) -> Self::Value {
-    let mut value = [0; 36];
-    self.consensus_encode(&mut value.as_mut_slice()).unwrap();
-    value
-  }
-}
-
-pub(super) type SatPointValue = [u8; 44];
-
-impl Entry for SatPoint {
-  type Value = SatPointValue;
-
-  fn load(value: Self::Value) -> Self {
-    Decodable::consensus_decode(&mut io::Cursor::new(value)).unwrap()
-  }
-
-  fn store(self) -> Self::Value {
-    let mut value = [0; 44];
-    self.consensus_encode(&mut value.as_mut_slice()).unwrap();
-    value
-  }
-}
-
-pub(super) type SatRange = (u64, u64);
-
-impl Entry for SatRange {
-  type Value = [u8; 11];
-
-  fn load([b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10]: Self::Value) -> Self {
-    let raw_base = u64::from_le_bytes([b0, b1, b2, b3, b4, b5, b6, 0]);
-
-    // 51 bit base
-    let base = raw_base & ((1 << 51) - 1);
-
-    let raw_delta = u64::from_le_bytes([b6, b7, b8, b9, b10, 0, 0, 0]);
-
-    // 33 bit delta
-    let delta = raw_delta >> 3;
-
-    (base, base + delta)
-  }
-
-  fn store(self) -> Self::Value {
-    let base = self.0;
-    let delta = self.1 - self.0;
-    let n = u128::from(base) | u128::from(delta) << 51;
-    n.to_le_bytes()[0..11].try_into().unwrap()
-  }
-}
-
-pub(super) type TxidValue = [u8; 32];
-
-impl Entry for Txid {
-  type Value = TxidValue;
-
-  fn load(value: Self::Value) -> Self {
-    Txid::from_inner(value)
-  }
-
-  fn store(self) -> Self::Value {
-    Txid::into_inner(self)
-  }
-}
-
 impl Entry for Option<InscriptionId> {
   type Value = (u128, u128, u32);
 
@@ -256,5 +183,78 @@ impl Entry for Option<InscriptionId> {
     } else {
       (0, 0, u32::MAX)
     }
+  }
+}
+
+pub(super) type OutPointValue = [u8; 36];
+
+impl Entry for OutPoint {
+  type Value = OutPointValue;
+
+  fn load(value: Self::Value) -> Self {
+    Decodable::consensus_decode(&mut io::Cursor::new(value)).unwrap()
+  }
+
+  fn store(self) -> Self::Value {
+    let mut value = [0; 36];
+    self.consensus_encode(&mut value.as_mut_slice()).unwrap();
+    value
+  }
+}
+
+pub(super) type SatPointValue = [u8; 44];
+
+impl Entry for SatPoint {
+  type Value = SatPointValue;
+
+  fn load(value: Self::Value) -> Self {
+    Decodable::consensus_decode(&mut io::Cursor::new(value)).unwrap()
+  }
+
+  fn store(self) -> Self::Value {
+    let mut value = [0; 44];
+    self.consensus_encode(&mut value.as_mut_slice()).unwrap();
+    value
+  }
+}
+
+pub(super) type SatRange = (u64, u64);
+
+impl Entry for SatRange {
+  type Value = [u8; 11];
+
+  fn load([b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10]: Self::Value) -> Self {
+    let raw_base = u64::from_le_bytes([b0, b1, b2, b3, b4, b5, b6, 0]);
+
+    // 51 bit base
+    let base = raw_base & ((1 << 51) - 1);
+
+    let raw_delta = u64::from_le_bytes([b6, b7, b8, b9, b10, 0, 0, 0]);
+
+    // 33 bit delta
+    let delta = raw_delta >> 3;
+
+    (base, base + delta)
+  }
+
+  fn store(self) -> Self::Value {
+    let base = self.0;
+    let delta = self.1 - self.0;
+    let n = u128::from(base) | u128::from(delta) << 51;
+    n.to_le_bytes()[0..11].try_into().unwrap()
+  }
+}
+
+pub(super) type TxidValue = [u8; 32];
+
+impl Entry for Txid {
+  type Value = TxidValue;
+
+  fn load(value: Self::Value) -> Self {
+    Txid::from_inner(value)
+  }
+
+  fn store(self) -> Self::Value {
+    Txid::into_inner(self)
   }
 }
