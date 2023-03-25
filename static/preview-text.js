4eee20ec67fa25007688fcd0c3c54b4e078cce47
Femi Olowonefa <feminefa@gmail.com>
2023-03-25 09:23:37 -0400

    added rest api for explorer

--- static/preview-text.js
+++ static/preview-text.js
@@ -29,9 +29,10 @@ function getMetaData(url) {
 let result = document.getElementById('text').innerText;
 try{
   result = JSON.stringify(JSON.parse(decodeHtml(result)), null, 2);
+  getMetaData(parseIpfsUrl(result.uri));
   console.log('RESULT', result);
 } catch(e) {
-    getMetaData(parseIpfsUrl(result.uri));
+  
 console.log('ERROR', e, decodeHtml(result));
 }
 document.getElementById('text').innerText='';
