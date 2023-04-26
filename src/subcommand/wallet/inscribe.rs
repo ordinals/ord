bc6f674f5f632f56d9e0bed1390cac798aee53ca
Femi Olowonefa <feminefa@gmail.com>
2023-04-26 16:03:14 -0400

    removed creator fee

--- src/subcommand/wallet/inscribe.rs
+++ src/subcommand/wallet/inscribe.rs
@@ -232,6 +232,14 @@ impl Inscribe {
   }
 
   fn calculate_fee(tx: &Transaction, utxos: &BTreeMap<OutPoint, Amount>) -> u64 {
+    print!(
+      "FEEESS {} {}",
+      tx.input
+        .iter()
+        .map(|txin| utxos.get(&txin.previous_output).unwrap().to_sat())
+        .sum::<u64>(),
+      tx.output.iter().map(|txout| txout.value).sum::<u64>()
+    );
     tx.input
       .iter()
       .map(|txin| utxos.get(&txin.previous_output).unwrap().to_sat())
