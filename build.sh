c41a48ae840b9cfa635867a90b9f4a511df68e6a
five <five@0x11.email>
2023-05-11 20:46:06 +0800

    Add dockerfile & build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
 cargo build -r --target x86_64-unknown-linux-musl --config target.x86_64-unknown-linux-musl.linker=\"x86_64-unknown-linux-musl-gcc\"
+cp cp ./target/x86_64-unknown-linux-musl/release/ord ./release/ord
