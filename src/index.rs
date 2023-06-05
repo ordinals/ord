77bcbdd2b1ff454babb848542e2bd7de1cfad998
Chrigeeel <tognazzachristian@gmail.com>
2023-06-05 18:24:57 +0200

    asd

--- src/index.rs
+++ src/index.rs
@@ -16,7 +16,7 @@ use {
   redb::{Database, ReadableTable, Table, TableDefinition, WriteStrategy, WriteTransaction},
   std::collections::HashMap,
   std::sync::atomic::{self, AtomicBool},
-  serde::ser::{Serialize};
+  serde::ser::{Serialize},
 };
 
 mod entry;
