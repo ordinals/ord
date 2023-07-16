267211c963ba6d0e6d0452c98a9cb87c513c2638
ordinally <hello@ordinally.net>
2023-07-16 19:01:16 +0200

    Format

--- src/index/updater/inscription_updater.rs
+++ src/index/updater/inscription_updater.rs
@@ -169,8 +169,7 @@ impl<'a, 'db, 'tx> InscriptionUpdater<'a, 'db, 'tx> {
         } else if inscription.tx_in_offset != 0 {
           Some(Curse::NotAtOffsetZero)
         } else if inscribed_offsets.contains_key(&offset) {
-          let seq_num = self
-            .reinscription_id_to_seq_num.len()?;
+          let seq_num = self.reinscription_id_to_seq_num.len()?;
 
           let sat = Self::calculate_sat(input_sat_ranges, offset);
           log::info!("processing reinscription {inscription_id} on sat {:?}: sequence number {seq_num}, inscribed offsets {:?}", sat, inscribed_offsets);
