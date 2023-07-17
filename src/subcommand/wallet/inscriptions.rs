6e21fa0c4333040b9f6ec8b7d16e942235e8ba2c
ordinally <hello@ordinally.net>
2023-07-17 13:11:17 +0200

    Finish merge

--- src/subcommand/wallet/inscriptions.rs
+++ src/subcommand/wallet/inscriptions.rs
@@ -11,8 +11,6 @@ pub(crate) fn run(options: Options) -> Result {
   let index = Index::open(&options)?;
   index.update()?;
 
-  // BACKLOG - change the logic here to only get inscriptions for outputs actually in the wallet
-
   let inscriptions = index.get_inscriptions(None)?;
   let unspent_outputs = index.get_unspent_outputs(Wallet::load(&options)?)?;
 
