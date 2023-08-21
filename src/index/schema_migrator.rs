c4bcb49a4949d175347196b072c45ce9cbf9a52b
ordinally <hello@ordinally.net>
2023-08-21 20:56:26 +0200

    Show content hash on inscription page More accurate time estimation for index migration

--- src/index/schema_migrator.rs
+++ src/index/schema_migrator.rs
@@ -33,12 +33,12 @@ impl SchemaMigrator {
     let mut content_hash_to_inscription_id =
       tx.open_multimap_table(CONTENT_HASH_TO_INSCRIPTION_ID)?;
 
-    let start_time = Instant::now();
-    let mut next_log = Duration::from_secs(30);
-
     let table = tx.open_table(INSCRIPTION_ID_TO_INSCRIPTION_ENTRY)?;
     let total_entries = table.len()?;
     let mut processed_entries = 0;
+
+    let start_time = Instant::now();
+    let mut next_log = Duration::from_secs(60);
 
     for result in table.iter()? {
       let (db_id, _) = result?;
@@ -95,12 +95,12 @@ impl SchemaMigrator {
       "Successfully migrated from schema version 5 to schema version 6 in {}s",
       start_time.elapsed().as_secs()
     );
-
     Ok(true)
   }
 
   fn migrate_6_to_5(database: &Database) -> std::result::Result<bool, Error> {
     log::info!("Migrating from schema version 6 to schema version 5...");
+
     let mut tx = database.begin_write()?;
     tx.set_durability(redb::Durability::Immediate);
     tx.delete_multimap_table(CONTENT_HASH_TO_INSCRIPTION_ID)?;
