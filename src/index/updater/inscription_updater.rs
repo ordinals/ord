d4896b9968c3a706dffee786082d5e8164ba7141
Casey Rodarmor <casey@rodarmor.com>
2023-09-20 14:17:02 -0700

    Fixy fixy!

--- src/index/updater/inscription_updater.rs
+++ src/index/updater/inscription_updater.rs
@@ -89,7 +89,7 @@ impl<'a, 'db, 'tx> InscriptionUpdater<'a, 'db, 'tx> {
 
     let next_sequence_number = sequence_number_to_id
       .iter()?
-      .next()
+      .next_back()
       .and_then(|result| result.ok())
       .map(|(number, _id)| number.value() + 1)
       .unwrap_or(0);
