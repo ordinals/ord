7a9f9a13c97cd4e545168d879a552e93998b5750
raphjaph <raphjaph@protonmail.com>
2023-09-25 22:52:43 +0200

    placate clippy

--- src/subcommand/wallet/inscribe.rs
+++ src/subcommand/wallet/inscribe.rs
@@ -187,11 +187,14 @@ impl Inscribe {
 
     let commit = client.send_raw_transaction(&signed_commit_tx)?;
 
-    let reveal = match client
-      .send_raw_transaction(&signed_reveal_tx) {
-        Ok(txid) => txid,
-        Err(err) => return Err(anyhow!("Failed to send reveal transaction: {err}\nCommit tx {commit} will be recovered once mined"))
-      };
+    let reveal = match client.send_raw_transaction(&signed_reveal_tx) {
+      Ok(txid) => txid,
+      Err(err) => {
+        return Err(anyhow!(
+        "Failed to send reveal transaction: {err}\nCommit tx {commit} will be recovered once mined"
+      ))
+      }
+    };
 
     Ok(Box::new(Output {
       commit,
