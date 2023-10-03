c67fa907c9e66d1ac964300af3aa6623f6187862
Casey Rodarmor <casey@rodarmor.com>
2023-10-03 11:28:53 -0700

    Sort inscribe arguments

--- src/subcommand/wallet/inscribe.rs
+++ src/subcommand/wallet/inscribe.rs
@@ -36,33 +36,6 @@ struct ParentInfo {
 
 #[derive(Debug, Parser)]
 pub(crate) struct Inscribe {
-  #[arg(long, help = "Inscribe <SATPOINT>.")]
-  pub(crate) satpoint: Option<SatPoint>,
-  #[arg(long, help = "Use fee rate of <FEE_RATE> sats/vB.")]
-  pub(crate) fee_rate: FeeRate,
-  #[arg(
-    long,
-    help = "Use <COMMIT_FEE_RATE> sats/vbyte for commit transaction.\nDefaults to <FEE_RATE> if unset."
-  )]
-  pub(crate) commit_fee_rate: Option<FeeRate>,
-  #[arg(help = "Inscribe sat with contents of <FILE>.")]
-  pub(crate) file: PathBuf,
-  #[arg(long, help = "Do not back up recovery key.")]
-  pub(crate) no_backup: bool,
-  #[arg(
-    long,
-    help = "Do not check that transactions are equal to or below the MAX_STANDARD_TX_WEIGHT of 400,000 weight units. Transactions over this limit are currently nonstandard and will not be relayed by bitcoind in its default configuration. Do not use this flag unless you understand the implications."
-  )]
-  pub(crate) no_limit: bool,
-  #[arg(long, help = "Don't sign or broadcast transactions.")]
-  pub(crate) dry_run: bool,
-  #[arg(long, help = "Send inscription to <DESTINATION>.")]
-  pub(crate) destination: Option<Address<NetworkUnchecked>>,
-  #[arg(
-    long,
-    help = "Amount of postage to include in the inscription. Default `10000sat`."
-  )]
-  pub(crate) postage: Option<Amount>,
   #[arg(
     long,
     help = "Include CBOR in file at <METADATA> as inscription metadata",
@@ -71,16 +44,43 @@ pub(crate) struct Inscribe {
   pub(crate) cbor_metadata: Option<PathBuf>,
   #[arg(
     long,
+    help = "Use <COMMIT_FEE_RATE> sats/vbyte for commit transaction.\nDefaults to <FEE_RATE> if unset."
+  )]
+  pub(crate) commit_fee_rate: Option<FeeRate>,
+  #[arg(long, help = "Send inscription to <DESTINATION>.")]
+  pub(crate) destination: Option<Address<NetworkUnchecked>>,
+  #[arg(long, help = "Don't sign or broadcast transactions.")]
+  pub(crate) dry_run: bool,
+  #[arg(long, help = "Use fee rate of <FEE_RATE> sats/vB.")]
+  pub(crate) fee_rate: FeeRate,
+  #[arg(help = "Inscribe sat with contents of <FILE>.")]
+  pub(crate) file: PathBuf,
+  #[arg(
+    long,
     help = "Include JSON in file at <METADATA> convered to CBOR as inscription metadata",
     conflicts_with = "cbor_metadata"
   )]
   pub(crate) json_metadata: Option<PathBuf>,
-  #[clap(long, help = "Make inscription a child of <PARENT>.")]
-  pub(crate) parent: Option<InscriptionId>,
-  #[clap(long, help = "Allow reinscription.")]
-  pub(crate) reinscribe: bool,
   #[clap(long, help = "Set inscription metaprotocol to <METAPROTOCOL>.")]
   pub(crate) metaprotocol: Option<String>,
+  #[arg(long, help = "Do not back up recovery key.")]
+  pub(crate) no_backup: bool,
+  #[arg(
+    long,
+    help = "Do not check that transactions are equal to or below the MAX_STANDARD_TX_WEIGHT of 400,000 weight units. Transactions over this limit are currently nonstandard and will not be relayed by bitcoind in its default configuration. Do not use this flag unless you understand the implications."
+  )]
+  pub(crate) no_limit: bool,
+  #[clap(long, help = "Make inscription a child of <PARENT>.")]
+  pub(crate) parent: Option<InscriptionId>,
+  #[arg(
+    long,
+    help = "Amount of postage to include in the inscription. Default `10000sat`."
+  )]
+  pub(crate) postage: Option<Amount>,
+  #[clap(long, help = "Allow reinscription.")]
+  pub(crate) reinscribe: bool,
+  #[arg(long, help = "Inscribe <SATPOINT>.")]
+  pub(crate) satpoint: Option<SatPoint>,
 }
 
 impl Inscribe {
