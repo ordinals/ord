0f6d15b38d198a2d0583a53aefe442040e3fefe0
Casey Rodarmor <casey@rodarmor.com>
2023-10-05 12:34:14 -0700

    Test runeid conversion

--- src/runes/rune_id.rs
+++ src/runes/rune_id.rs
@@ -9,3 +9,20 @@ impl From<RuneId> for u128 {
     u128::from(id.height) << 16 | u128::from(id.index)
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn rune_id_to_128() {
+    assert_eq!(
+      0b11_0000_0000_0000_0001u128,
+      RuneId {
+        height: 3,
+        index: 1,
+      }
+      .into()
+    );
+  }
+}
