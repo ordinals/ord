3408de49028d55cfcd487af624e78f4641a71bc8
Casey Rodarmor <casey@rodarmor.com>
2023-10-10 11:31:04 -0700

    Format rune supply using divisibility (#2509)

--- src/runes.rs
+++ src/runes.rs
@@ -1,12 +1,15 @@
 use {self::error::Error, super::*};
 
 pub(crate) use {
-  edict::Edict, etching::Etching, rune::Rune, rune_id::RuneId, runestone::Runestone,
+  edict::Edict, etching::Etching, pile::Pile, rune::Rune, rune_id::RuneId, runestone::Runestone,
 };
+
+const MAX_DIVISIBILITY: u8 = 38;
 
 mod edict;
 mod error;
 mod etching;
+mod pile;
 mod rune;
 mod rune_id;
 mod runestone;
--- src/runes/etching.rs
+++ src/runes/etching.rs
@@ -2,6 +2,6 @@ use super::*;
 
 #[derive(Default, Serialize, Debug, PartialEq, Copy, Clone)]
 pub struct Etching {
-  pub(crate) divisibility: u128,
+  pub(crate) divisibility: u8,
   pub(crate) rune: Rune,
 }
