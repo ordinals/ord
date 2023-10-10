882d6a84a3db68c99eb28b28ee94ebbfc21dbea5
raphjaph <raphjaph@protonmail.com>
2023-10-10 17:03:36 +0200

    add mode for real

---
+++ src/subcommand/wallet/inscribe/mode.rs
@@ -0,0 +1,52 @@
+use super::*;
+
+#[derive(PartialEq, Debug, Default)]
+pub(crate) enum Mode {
+  SharedOutput,
+  #[default]
+  SeparateOutputs,
+}
+
+impl Display for Mode {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(
+      f,
+      "{}",
+      match self {
+        Mode::SharedOutput => "shared-output",
+        Mode::SeparateOutputs => "separate-outputs",
+      }
+    )
+  }
+}
+
+impl FromStr for Mode {
+  type Err = String;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    match s {
+      "shared-output" => Ok(Mode::SharedOutput),
+      "separate-outputs" => Ok(Mode::SeparateOutputs),
+      _ => Err(format!("'{}' is not a valid Mode", s)),
+    }
+  }
+}
+
+impl Serialize for Mode {
+  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+  where
+    S: Serializer,
+  {
+    serializer.serialize_str(&self.to_string())
+  }
+}
+
+impl<'de> Deserialize<'de> for Mode {
+  fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+  where
+    D: Deserializer<'de>,
+  {
+    let s = String::deserialize(deserializer)?;
+    Mode::from_str(&s).map_err(serde::de::Error::custom)
+  }
+}
