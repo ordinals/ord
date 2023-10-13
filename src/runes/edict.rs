95c3fe1b5725a061c8bcebf89f164b63d480780c
Casey Rodarmor <casey@rodarmor.com>
2023-10-12 23:52:23 -0700

    Add runestone size test

--- src/runes/edict.rs
+++ src/runes/edict.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-#[derive(Default, Serialize, Debug, PartialEq, Clone)]
+#[derive(Default, Serialize, Debug, PartialEq, Copy, Clone)]
 pub struct Edict {
   pub id: u128,
   pub amount: u128,
