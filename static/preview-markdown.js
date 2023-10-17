0841a5b71129bc59cf88fdf5e2b0b9d290a21295
bop.xbt <bots@boppleton.com>
2023-10-17 13:03:39 -0400

    replace preview-markdown dep

--- static/preview-markdown.js
+++ static/preview-markdown.js
@@ -1,4 +1,4 @@
-import { marked } from 'https://cdn.jsdelivr.net/npm/marked@9/+esm'
+import { marked } from '/content/cf7ef67632861da5a59a716ad5ee398054f455c0011a7b6aa12c5985c1b8b0b8i0'
 
 const inscription = document.documentElement.dataset.inscription;
 const response = await fetch(`/content/${inscription}`);
