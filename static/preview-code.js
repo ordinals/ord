32f5c18d48754aeae061805f87ce1724d181d174
bop.xbt <bots@boppleton.com>
2023-10-17 13:02:32 -0400

    replace preview-code.js deps

--- static/preview-code.js
+++ static/preview-code.js
@@ -1,8 +1,8 @@
-import hljs from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11/build/es/highlight.min.js';
-import javascript from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11/build/es/languages/javascript.min.js';
-import yaml from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11/build/es/languages/yaml.min.js';
-import css from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11/build/es/languages/css.min.js';
-import json from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11/build/es/languages/json.min.js';
+import hljs from '/content/e26e96915acbaed1888fdeb72402e9eb21f83e533ee26fc7101e631b3f78eae6i0';
+import javascript from '/content/7a115d8cb6f6f9feeca922cdeed30368db49d97ccb607ce4bcd6f94a7917555fi0';
+import yaml from '/content/0430c866bc414f8e744bb23aabc668c23ab8b71071e533fb4ee6e0832fb5cc22i0';
+import css from '/content/ca08f3ab2402161e6efdc2f4031793c237061fb2c9f561a0830618acdf970d5fi0';
+import json from '/content/ea8e6708b2ba4fb5cc21ef21c330b6e650ca04ca01d02d3462ef7d250ec27eb4i0';
 
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('yaml', yaml);
