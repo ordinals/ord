e30fc0e09f0e928fc2cdd9eac77b34118aea6565
Casey Rodarmor <casey@rodarmor.com>
2023-10-22 11:33:22 -0700

    Clean up implementation

--- src/teleburn_address.rs
+++ src/teleburn_address.rs
@@ -17,26 +17,27 @@ impl From<InscriptionId> for EthereumTeleburnAddress {
 /// Given an Ethereum address, return that address with a checksum
 /// as per https://eips.ethereum.org/EIPS/eip-55
 fn create_address_with_checksum(addr: &str) -> String {
-  let normalized_addr = addr.trim_start_matches("0x").to_ascii_lowercase();
   let mut hasher = Sha3::keccak256();
-  hasher.input(normalized_addr.as_bytes());
+  hasher.input(addr.as_bytes());
   let hashed_addr = hasher.result_str();
-  normalized_addr
+  addr
     .char_indices()
-    .fold("0x".to_string(), |mut buff, (idx, c)| {
-      if c.is_numeric() {
-        buff.push(c);
-      } else {
-        // safe to unwrap because we have a hex string
-        let value =
-          u8::from_str_radix(&hashed_addr.chars().nth(idx).unwrap().to_string(), 16).unwrap();
-        if value > 7 {
-          buff.push(c.to_ascii_uppercase());
-        } else {
-          buff.push(c);
+    .fold("0x".to_string(), |mut buf, (idx, c)| {
+      buf.push(match c {
+        '0'..='9' => c,
+        'a'..='f' => {
+          // safe to unwrap because we have a hex string
+          let value =
+            u8::from_str_radix(&hashed_addr.chars().nth(idx).unwrap().to_string(), 16).unwrap();
+          if value > 7 {
+            c.to_ascii_uppercase()
+          } else {
+            c
+          }
         }
-      }
-      buff
+        _ => unreachable!(),
+      });
+      buf
     })
 }
 
@@ -59,7 +60,7 @@ mod tests {
       "0xdbF03B407c01E7cD3CBea99509d93f8DDDC8C6FB",
       "0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb",
     ] {
-      let lowercased = String::from(*addr).to_ascii_lowercase();
+      let lowercased = String::from(&addr[2..]).to_ascii_lowercase();
       assert_eq!(addr.to_string(), create_address_with_checksum(&lowercased));
     }
   }
