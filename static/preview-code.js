231bdda4a621603bd9807c90fddef2bb91c9c263
Eloc <42568538+elocremarc@users.noreply.github.com>
2023-10-25 17:45:06 -0700

    Add syntax highlighting for Python inscriptions (#2538)

--- static/preview-code.js
+++ static/preview-code.js
@@ -1,18 +1,22 @@
 import hljs from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11/build/es/highlight.min.js';
-import javascript from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11/build/es/languages/javascript.min.js';
-import yaml from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11/build/es/languages/yaml.min.js';
 import css from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11/build/es/languages/css.min.js';
+import javascript from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11/build/es/languages/javascript.min.js';
 import json from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11/build/es/languages/json.min.js';
+import python from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11/build/es/languages/python.min.js';
+import yaml from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11/build/es/languages/yaml.min.js';
 
-hljs.registerLanguage('javascript', javascript);
-hljs.registerLanguage('yaml', yaml);
 hljs.registerLanguage('css', css);
+hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('json', json);
+hljs.registerLanguage('python', python);
+hljs.registerLanguage('yaml', yaml);
 
 const inscription = document.documentElement.dataset.inscription;
 const response = await fetch(`/content/${inscription}`);
 const contentType = response.headers.get("content-type");
-const language = contentType.split("/")[1];
+let language = contentType.split("/")[1];
+if (language === "x-python") {
+    language = "python";
+}
 const code = await response.text();
-
 document.body.innerHTML = `<pre><code>${hljs.highlight(code, {language, ignoreIllegals: true}).value}</code></pre>`;
