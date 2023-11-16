61de4a2fed7794e517a51463d1bb399b9bc477e1
Casey Rodarmor <casey@rodarmor.com>
2023-11-16 13:21:06 -0800

    Update schema version for charms

--- src/index.rs
+++ src/index.rs
@@ -35,7 +35,7 @@ mod updater;
 #[cfg(test)]
 pub(crate) mod testing;
 
-const SCHEMA_VERSION: u64 = 10;
+const SCHEMA_VERSION: u64 = 11;
 
 macro_rules! define_table {
   ($name:ident, $key:ty, $value:ty) => {
