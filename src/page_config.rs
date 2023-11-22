58beb64b0f70aa1b7603da64e994b1b712ffe0ff
Casey Rodarmor <casey@rodarmor.com>
2023-11-22 10:15:54 -0800

    Derive default for page config

--- src/page_config.rs
+++ src/page_config.rs
@@ -1,20 +1,9 @@
 use super::*;
 
-#[derive(Clone)]
+#[derive(Clone, Default)]
 pub(crate) struct PageConfig {
   pub(crate) chain: Chain,
   pub(crate) domain: Option<String>,
   pub(crate) index_sats: bool,
   pub(crate) content_security_policy_origin: Option<String>,
-}
-
-impl Default for PageConfig {
-  fn default() -> Self {
-    Self {
-      chain: Chain::Mainnet,
-      domain: None,
-      index_sats: false,
-      content_security_policy_origin: None,
-    }
-  }
 }
