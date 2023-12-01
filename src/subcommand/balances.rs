d8a4ca066a62001933970dcf2201d0095a0a007b
Casey Rodarmor <casey@rodarmor.com>
2023-12-01 12:34:46 -0800

    Add `ord balances` to list all rune balances

---
+++ src/subcommand/balances.rs
@@ -0,0 +1,25 @@
+use super::*;
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Output {
+  pub runes: BTreeMap<Rune, BTreeMap<OutPoint, u128>>,
+}
+
+pub(crate) fn run(options: Options) -> SubcommandResult {
+  let index = Index::open(&options)?;
+
+  ensure!(
+    index.has_rune_index(),
+    "`ord balances` requires index created with `--index-runes-pre-alpha-i-agree-to-get-rekt` flag",
+  );
+
+  index.update()?;
+
+  Ok(Box::new(Output {
+    runes: index
+      .get_rune_balance_map()?
+      .into_iter()
+      .map(|(outpoint, balances)| (outpoint, balances.into_iter().collect()))
+      .collect(),
+  }))
+}
