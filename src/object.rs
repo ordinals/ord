80d65f262ac91455a8ac46444c7c02cab9767224
Casey Rodarmor <casey@rodarmor.com>
2023-12-11 14:50:54 -0800

    Add runes to parse command (#2830)

--- src/object.rs
+++ src/object.rs
@@ -7,6 +7,7 @@ pub enum Object {
   InscriptionId(InscriptionId),
   Integer(u128),
   OutPoint(OutPoint),
+  Rune(Rune),
   Sat(Sat),
   SatPoint(SatPoint),
 }
@@ -26,6 +27,7 @@ impl FromStr for Object {
       InscriptionId => Ok(Self::InscriptionId(s.parse()?)),
       Integer => Ok(Self::Integer(s.parse()?)),
       OutPoint => Ok(Self::OutPoint(s.parse()?)),
+      Rune => Ok(Self::Rune(s.parse()?)),
       SatPoint => Ok(Self::SatPoint(s.parse()?)),
     }
   }
@@ -44,6 +46,7 @@ impl Display for Object {
       Self::InscriptionId(inscription_id) => write!(f, "{inscription_id}"),
       Self::Integer(integer) => write!(f, "{integer}"),
       Self::OutPoint(outpoint) => write!(f, "{outpoint}"),
+      Self::Rune(rune) => write!(f, "{rune}"),
       Self::Sat(sat) => write!(f, "{sat}"),
       Self::SatPoint(satpoint) => write!(f, "{satpoint}"),
     }
@@ -196,5 +199,7 @@ mod tests {
           .unwrap(),
       ),
     );
+    case("A", Object::Rune(Rune(0)));
+    case("B", Object::Rune(Rune(1)));
   }
 }
