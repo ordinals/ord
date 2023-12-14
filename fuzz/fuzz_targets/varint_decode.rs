3187b7aa8d68ac1b83a80d8ab837a35171fe7fab
Casey Rodarmor <casey@rodarmor.com>
2023-12-14 10:13:20 +0800

    tweak

--- fuzz/fuzz_targets/varint_decode.rs
+++ fuzz/fuzz_targets/varint_decode.rs
@@ -6,9 +6,7 @@ fuzz_target!(|input: &[u8]| {
   let mut i = 0;
 
   while i < input.len() {
-    let Ok((decoded, length)) = varint::decode(&input[i..]) else {
-      break;
-    };
+    let (decoded, length) = varint::decode(&input[i..]);
     let mut encoded = Vec::new();
     varint::encode_to_vec(decoded, &mut encoded);
     assert_eq!(encoded, &input[i..i + length]);
