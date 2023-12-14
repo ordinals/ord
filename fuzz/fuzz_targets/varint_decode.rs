78c76dd73c80fea4134b83792228dfd776b371b8
Casey Rodarmor <casey@rodarmor.com>
2023-12-13 19:11:27 -0800

    Fix fuzzers (#2859)

--- fuzz/fuzz_targets/varint_decode.rs
+++ fuzz/fuzz_targets/varint_decode.rs
@@ -6,12 +6,11 @@ fuzz_target!(|input: &[u8]| {
   let mut i = 0;
 
   while i < input.len() {
-    let Ok((decoded, length)) = varint::decode(&input[i..]) else {
-      break;
-    };
+    let (decoded, length) = varint::decode(&input[i..]);
     let mut encoded = Vec::new();
     varint::encode_to_vec(decoded, &mut encoded);
-    assert_eq!(encoded, &input[i..i + length]);
+    let (redecoded, _) = varint::decode(&input[i..]);
+    assert_eq!(redecoded, decoded);
     i += length;
   }
 });
