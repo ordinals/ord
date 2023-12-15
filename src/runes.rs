8b20495c05d482bc72a11e3b90d93255b4070f2a
Casey Rodarmor <casey@rodarmor.com>
2023-12-15 13:39:08 +0800

    Unlock runes in first block of interval (#2861)

--- src/runes.rs
+++ src/runes.rs
@@ -171,7 +171,7 @@ mod tests {
 
   #[test]
   fn runes_must_be_greater_than_or_equal_to_minimum_for_height() {
-    const SECOND_BLOCK_LOCKED_RUNE: u128 = 99235208761673842;
+    let block_two_minimum: u128 = Rune::minimum_at_height(Chain::Regtest, Height(2)).0;
 
     {
       let context = Context::builder()
@@ -191,7 +191,7 @@ mod tests {
               output: 0,
             }],
             etching: Some(Etching {
-              rune: Some(Rune(SECOND_BLOCK_LOCKED_RUNE - 1)),
+              rune: Some(Rune(block_two_minimum - 1)),
               ..Default::default()
             }),
             ..Default::default()
@@ -221,7 +221,7 @@ mod tests {
               output: 0,
             }],
             etching: Some(Etching {
-              rune: Some(Rune(SECOND_BLOCK_LOCKED_RUNE)),
+              rune: Some(Rune(block_two_minimum)),
               ..Default::default()
             }),
             ..Default::default()
@@ -243,7 +243,7 @@ mod tests {
           id,
           RuneEntry {
             etching: txid,
-            rune: Rune(SECOND_BLOCK_LOCKED_RUNE),
+            rune: Rune(block_two_minimum),
             supply: u128::max_value(),
             timestamp: 2,
             ..Default::default()
