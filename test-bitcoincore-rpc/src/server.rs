e44cce8fec02a31b3e67f4460ecc840e4786a944
Casey Rodarmor <casey@rodarmor.com>
2023-12-16 09:04:47 +0800

    Use pre-segwit transaction serialization for fundrawtransaction (#2865)

--- test-bitcoincore-rpc/src/server.rs
+++ test-bitcoincore-rpc/src/server.rs
@@ -1,9 +1,11 @@
 use {
   super::*,
   bitcoin::{
+    consensus::Decodable,
     secp256k1::{rand, KeyPair, Secp256k1, XOnlyPublicKey},
     Witness,
   },
+  std::io::Cursor,
 };
 
 pub(crate) struct Server {
@@ -243,7 +245,26 @@ impl Api for Server {
     options: Option<FundRawTransactionOptions>,
     _is_witness: Option<bool>,
   ) -> Result<FundRawTransactionResult, jsonrpc_core::Error> {
-    let mut transaction: Transaction = deserialize(&hex::decode(tx).unwrap()).unwrap();
+    let options = options.unwrap();
+
+    let mut cursor = Cursor::new(hex::decode(tx).unwrap());
+
+    let version = i32::consensus_decode_from_finite_reader(&mut cursor).unwrap();
+    let input = Vec::<TxIn>::consensus_decode_from_finite_reader(&mut cursor).unwrap();
+    let output = Decodable::consensus_decode_from_finite_reader(&mut cursor).unwrap();
+    let lock_time = Decodable::consensus_decode_from_finite_reader(&mut cursor).unwrap();
+
+    let mut transaction = Transaction {
+      version,
+      input,
+      output,
+      lock_time,
+    };
+
+    assert_eq!(
+      options.change_position,
+      Some(transaction.output.len().try_into().unwrap())
+    );
 
     let state = self.state();
 
@@ -294,7 +315,7 @@ impl Api for Server {
       script_pubkey: ScriptBuf::new(),
     });
 
-    let fee = if let Some(fee_rate) = options.and_then(|options| options.fee_rate) {
+    let fee = if let Some(fee_rate) = options.fee_rate {
       // increase vsize to account for the witness that `fundrawtransaction` will add
       let funded_vsize = transaction.vsize() as f64 + 68.0 / 4.0;
       let funded_kwu = funded_vsize / 1000.0;
