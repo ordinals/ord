d23621773404778cac2d26560772eca4856f1fb7
Casey Rodarmor <casey@rodarmor.com>
2023-12-25 12:42:29 -0800

    Bump schema

--- src/index.rs
+++ src/index.rs
@@ -41,7 +41,7 @@ mod updater;
 #[cfg(test)]
 pub(crate) mod testing;
 
-const SCHEMA_VERSION: u64 = 15;
+const SCHEMA_VERSION: u64 = 16;
 
 macro_rules! define_table {
   ($name:ident, $key:ty, $value:ty) => {
