a14ef0a483e517fd7b99a4199d9613ce98c9d90d
raphjaph <raphjaph@protonmail.com>
2024-01-03 18:57:22 +0100

    Fix wallet create

--- src/subcommand/wallet.rs
+++ src/subcommand/wallet.rs
@@ -158,11 +158,17 @@ pub(crate) fn get_change_address(client: &Client, chain: Chain) -> Result<Addres
 }
 
 pub(crate) fn initialize(wallet: String, options: &Options, seed: [u8; 64]) -> Result {
-  let client = check_version(options.bitcoin_rpc_client(None)?)?;
+  check_version(options.bitcoin_rpc_client(None)?)?.create_wallet(
+    &wallet,
+    None,
+    Some(true),
+    None,
+    None,
+  )?;
+
+  let client = bitcoin_rpc_client_for_wallet_command(wallet, options)?;
 
   let network = options.chain().network();
-
-  client.create_wallet(&wallet, None, Some(true), None, None)?;
 
   let secp = Secp256k1::new();
 
