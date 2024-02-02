45c8c726570c8618f21c4726112ee1dbc8306896
Casey Rodarmor <casey@rodarmor.com>
2024-02-02 14:13:19 -0800

    Improve dfs

--- crates/ordinals/src/deserialize_from_str.rs
+++ crates/ordinals/src/deserialize_from_str.rs
@@ -2,6 +2,18 @@ use super::*;
 
 pub struct DeserializeFromStr<T: FromStr>(pub T);
 
+impl<'de, T: FromStr> DeserializeFromStr<T>
+where
+  T::Err: Display,
+{
+  pub fn with<D>(deserializer: D) -> Result<T, D::Error>
+  where
+    D: Deserializer<'de>,
+  {
+    Ok(DeserializeFromStr::<T>::deserialize(deserializer)?.0)
+  }
+}
+
 impl<'de, T: FromStr> Deserialize<'de> for DeserializeFromStr<T>
 where
   T::Err: Display,
--- crates/ordinals/src/sat_point.rs
+++ crates/ordinals/src/sat_point.rs
@@ -53,7 +53,7 @@ impl<'de> Deserialize<'de> for SatPoint {
   where
     D: Deserializer<'de>,
   {
-    Ok(DeserializeFromStr::deserialize(deserializer)?.0)
+    DeserializeFromStr::with(deserializer)
   }
 }
 
