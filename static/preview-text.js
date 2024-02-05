1d2cb78d97cc3bc8b05035a1ea1052ce504a1c35
oxSaturn <oxSaturn@proton.me>
2024-02-05 22:02:59 +0800

    Optimize preview text

--- static/preview-text.js
+++ static/preview-text.js
@@ -3,12 +3,15 @@ const inscription = document.documentElement.dataset.inscription;
 const response = await fetch(`/content/${inscription}`);
 const text = await response.text();
 for (const pre of document.querySelectorAll('pre')) {
-  pre.textContent = text;
+  try {
+    pre.textContent = JSON.stringify(JSON.parse(text), null, 2);
+  } catch (e) {
+    pre.textContent = text;
+  }
 }
 
 let pre = document.querySelector('body > pre');
-let { width, height } = pre.getBoundingClientRect();
-let columns = width / 16;
-let rows = height / 16;
-pre.style.fontSize = `min(${95/columns}vw, ${95/rows}vh)`;
+pre.style.fontSize = '12px';
 pre.style.opacity = 1;
+pre.style.lineHeight = '1.2';
+pre.style.whiteSpace = 'pre-wrap';
