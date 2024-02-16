adf021eec0ca977ad70a177a8656a9fb236fa5c6
raphjaph <raphjaph@protonmail.com>
2024-02-15 17:13:47 -0800

    Amend

--- src/subcommand/wallet/restore.rs
+++ src/subcommand/wallet/restore.rs
@@ -1,20 +1,17 @@
 use super::*;
 
 #[derive(Debug, Parser)]
-#[clap(group(
-  ArgGroup::new("source").required(true).args(&["descriptor", "mnemonic"]))
-)]
 pub(crate) struct Restore {
-  #[arg(long, help = "Restore wallet from <DESCRIPTOR> from stdin.")]
-  descriptor: bool,
-  #[arg(long, help = "Restore wallet from <MNEMONIC> from stdin.")]
-  mnemonic: bool,
-  #[arg(
-    long,
-    requires = "mnemonic",
-    help = "Use <PASSPHRASE> when deriving wallet"
-  )]
+  #[clap(value_enum, long, help = "Restore wallet from <SOURCE> on stdin.")]
+  from: Source,
+  #[arg(long, help = "Use <PASSPHRASE> when deriving wallet")]
   pub(crate) passphrase: Option<String>,
+}
+
+#[derive(clap::ValueEnum, Debug, Clone)]
+enum Source {
+  Descriptor,
+  Mnemonic,
 }
 
 impl Restore {
@@ -24,31 +21,21 @@ impl Restore {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
 
-    if self.descriptor {
-      let wallet_descriptors: ListDescriptorsResult = serde_json::from_str(&buffer)?;
-      wallet.initialize_from_descriptors(wallet_descriptors.descriptors)?;
-    } else if self.mnemonic {
-      let mnemonic = Mnemonic::parse_normalized(&buffer)?;
-      wallet.initialize(mnemonic.to_seed(self.passphrase.unwrap_or_default()))?
-    } else {
-      unreachable!()
+    match self.from {
+      Source::Descriptor => {
+        ensure!(
+          self.passphrase.is_none(),
+          "descriptor does not take a passphrase"
+        );
+        let wallet_descriptors: ListDescriptorsResult = serde_json::from_str(&buffer)?;
+        wallet.initialize_from_descriptors(wallet_descriptors.descriptors)?;
+      }
+      Source::Mnemonic => {
+        let mnemonic = Mnemonic::parse_normalized(&buffer)?;
+        wallet.initialize(mnemonic.to_seed(self.passphrase.unwrap_or_default()))?;
+      }
     }
 
     Ok(None)
-  }
-}
-
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test]
-  fn descriptor_and_mnemonic_conflict() {
-    assert_regex_match!(
-      Arguments::try_parse_from(["ord", "wallet", "restore", "--descriptor", "--mnemonic",])
-        .unwrap_err()
-        .to_string(),
-      ".*--descriptor.*cannot be used with.*--mnemonic.*"
-    );
   }
 }
