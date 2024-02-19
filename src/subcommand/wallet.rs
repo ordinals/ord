1569ad215a7ef0d41c6be15dd15bebd84f26b18e
raphjaph <raphjaph@protonmail.com>
2024-02-19 15:48:27 -0800

    make prettier

--- src/subcommand/wallet.rs
+++ src/subcommand/wallet.rs
@@ -72,31 +72,30 @@ pub(crate) enum Subcommand {
 impl WalletCommand {
   pub(crate) fn run(self, options: Options) -> SubcommandResult {
     match self.subcommand {
-      Subcommand::Create(create) => create.run(self.name, &options),
-      Subcommand::Restore(restore) => restore.run(self.name, &options),
-      _ => {
-        let wallet = Wallet::build(
-          self.name.clone(),
-          self.no_sync,
-          options.clone(),
-          self.server_url,
-        )?;
+      Subcommand::Create(create) => return create.run(self.name, &options),
+      Subcommand::Restore(restore) => return restore.run(self.name, &options),
+      _ => {}
+    };
+    let wallet = Wallet::build(
+      self.name.clone(),
+      self.no_sync,
+      options.clone(),
+      self.server_url,
+    )?;
 
-        match self.subcommand {
-          Subcommand::Balance => balance::run(wallet),
-          Subcommand::Dump => dump::run(wallet),
-          Subcommand::Etch(etch) => etch.run(wallet),
-          Subcommand::Inscribe(inscribe) => inscribe.run(wallet),
-          Subcommand::Inscriptions => inscriptions::run(wallet),
-          Subcommand::Receive => receive::run(wallet),
-          Subcommand::Sats(sats) => sats.run(wallet),
-          Subcommand::Send(send) => send.run(wallet),
-          Subcommand::Transactions(transactions) => transactions.run(wallet),
-          Subcommand::Outputs => outputs::run(wallet),
-          Subcommand::Cardinals => cardinals::run(wallet),
-          _ => unreachable!(),
-        }
-      }
+    match self.subcommand {
+      Subcommand::Balance => balance::run(wallet),
+      Subcommand::Dump => dump::run(wallet),
+      Subcommand::Etch(etch) => etch.run(wallet),
+      Subcommand::Inscribe(inscribe) => inscribe.run(wallet),
+      Subcommand::Inscriptions => inscriptions::run(wallet),
+      Subcommand::Receive => receive::run(wallet),
+      Subcommand::Sats(sats) => sats.run(wallet),
+      Subcommand::Send(send) => send.run(wallet),
+      Subcommand::Transactions(transactions) => transactions.run(wallet),
+      Subcommand::Outputs => outputs::run(wallet),
+      Subcommand::Cardinals => cardinals::run(wallet),
+      Subcommand::Create(_) | Subcommand::Restore(_) => unreachable!(),
     }
   }
 }
