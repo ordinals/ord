b37e3392a2c454d8013d3924450835ed108da478
Arik Sosman <git@arik.io>
2024-03-06 13:09:44 -0800

    Update schema version.

--- src/index.rs
+++ src/index.rs
@@ -43,7 +43,7 @@ mod updater;
 #[cfg(test)]
 pub(crate) mod testing;
 
-const SCHEMA_VERSION: u64 = 18;
+const SCHEMA_VERSION: u64 = 19;
 
 macro_rules! define_table {
   ($name:ident, $key:ty, $value:ty) => {
