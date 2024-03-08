7ece7d8efd0427943400ea803d84c29d3f0a0772
cliobitcoinbank <128262796+cliobitcoinbank@users.noreply.github.com>
2024-03-07 19:47:35 -0500

    Update dump.rs for public descriptor dumping.
    Added ability to dump public keys by default instead of private keys, needs --dump_private_keys for the private keys to dump instead.

--- src/subcommand/wallet/dump.rs
+++ src/subcommand/wallet/dump.rs
@@ -1,14 +1,25 @@
 use super::*;
+use std::collections::BTreeMap;
 
-pub(crate) fn run(wallet: Wallet) -> SubcommandResult {
-  eprintln!(
-    "==========================================
-= THIS STRING CONTAINS YOUR PRIVATE KEYS =
-=        DO NOT SHARE WITH ANYONE        =
-=========================================="
-  );
+pub(crate) fn run(wallet: Wallet, matches: &ArgMatches) -> SubcommandResult {
+    let dump_private_keys = matches.is_present("dump-private-keys");
 
-  Ok(Some(Box::new(
-    wallet.bitcoin_client().list_descriptors(Some(true))?,
-  )))
+    let warning_message = if dump_private_keys {
+        "==========================================\n\
+         = THIS STRING CONTAINS YOUR PRIVATE KEYS =\n\
+         =        DO NOT SHARE WITH ANYONE        =\n\
+         =========================================="
+    } else {
+        "==========================================\n\
+         =        PUBLIC DESCRIPTOR OUTPUT        =\n\
+         =       HANDLE WITH CARE, BUT LESS       =\n\
+         =       SENSITIVE THAN PRIVATE KEYS      =\n\
+         =========================================="
+    };
+
+    eprintln!(warning_message);
+
+    Ok(Some(Box::new(
+        wallet.bitcoin_client().list_descriptors(Some(dump_private_keys))?,
+    )))
 }
