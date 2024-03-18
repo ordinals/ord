a6a4f74b9cc94ba0f04d5758db54442c89a8f30c
Casey Rodarmor <casey@rodarmor.com>
2024-03-17 20:02:05 -0700

    Tweak

--- src/index.rs
+++ src/index.rs
@@ -48,7 +48,7 @@ mod updater;
 #[cfg(test)]
 pub(crate) mod testing;
 
-const SCHEMA_VERSION: u64 = 19;
+const SCHEMA_VERSION: u64 = 20;
 
 macro_rules! define_table {
   ($name:ident, $key:ty, $value:ty) => {
