c5cfe4e9c0259f8c91ee3373e40e7b911545b180
Casey Rodarmor <casey@rodarmor.com>
2024-03-22 17:46:25 -0700

    Only allow sending sats by name (#3344)

--- src/re.rs
+++ src/re.rs
@@ -11,6 +11,17 @@ lazy_static! {
   pub(crate) static ref OUTPOINT: Regex = re(r"[[:xdigit:]]{64}:\d+");
   pub(crate) static ref RUNE_ID: Regex = re(r"[0-9]+:[0-9]+");
   pub(crate) static ref SATPOINT: Regex = re(r"[[:xdigit:]]{64}:\d+:\d+");
-  pub(crate) static ref SAT_NAME: Regex = re(r"[a-z]+");
+  pub(crate) static ref SAT_NAME: Regex = re(r"[a-z]{1,11}");
   pub(crate) static ref SPACED_RUNE: Regex = re(r"[A-Z•.]+");
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn sat_name() {
+    assert!(SAT_NAME.is_match(&Sat(0).name()));
+    assert!(SAT_NAME.is_match(&Sat::LAST.name()));
+  }
 }
