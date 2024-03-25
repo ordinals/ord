8a95c3626d89c685c35961109da4b2b6b6cd7a54
Casey Rodarmor <casey@rodarmor.com>
2024-03-25 15:04:23 -0700

    Test supply error

--- tests/wallet/inscribe.rs
+++ tests/wallet/inscribe.rs
@@ -3001,3 +3001,52 @@ fn etch_mintable_plus_premine_overflow_error() {
     .expected_exit_code(1)
     .run_and_extract_stdout();
 }
+
+#[test]
+fn incorrect_supply_error() {
+  let bitcoin_rpc_server = test_bitcoincore_rpc::builder()
+    .network(Network::Regtest)
+    .build();
+
+  let ord_rpc_server =
+    TestServer::spawn_with_server_args(&bitcoin_rpc_server, &["--regtest", "--index-runes"], &[]);
+
+  create_wallet(&bitcoin_rpc_server, &ord_rpc_server);
+
+  bitcoin_rpc_server.mine_blocks(1);
+
+  CommandBuilder::new("--regtest --index-runes wallet inscribe --fee-rate 0 --batch batch.yaml")
+    .write("inscription.txt", "foo")
+    .write(
+      "batch.yaml",
+      serde_yaml::to_string(&batch::File {
+        etching: Some(batch::Etching {
+          divisibility: 0,
+          rune: SpacedRune {
+            rune: Rune(RUNE),
+            spacers: 0,
+          },
+          supply: "1".parse().unwrap(),
+          premine: "1".parse().unwrap(),
+          symbol: '¢',
+          mint: Some(batch::Mint {
+            cap: 1,
+            term: Some(2),
+            limit: "1".parse().unwrap(),
+            deadline: None,
+          }),
+        }),
+        inscriptions: vec![batch::Entry {
+          file: "inscription.txt".into(),
+          ..default()
+        }],
+        ..default()
+      })
+      .unwrap(),
+    )
+    .bitcoin_rpc_server(&bitcoin_rpc_server)
+    .ord_rpc_server(&ord_rpc_server)
+    .expected_stderr("error: `supply` not equal to `premine` + `mint.count` * `mint.limit`\n")
+    .expected_exit_code(1)
+    .run_and_extract_stdout();
+}
