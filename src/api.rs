c61b99c320ec4c940e702d60969a1942dd4df1c8
Arik <arik-so@users.noreply.github.com>
2024-03-27 15:07:20 -0700

    Return effective content type in JSON API (#3289)

--- src/api.rs
+++ src/api.rs
@@ -80,6 +80,7 @@ pub struct Inscription {
   pub children: Vec<InscriptionId>,
   pub content_length: Option<usize>,
   pub content_type: Option<String>,
+  pub effective_content_type: Option<String>,
   pub fee: u64,
   pub height: u32,
   pub id: InscriptionId,
