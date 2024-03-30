167cf1614d524fe7225c8e7de717a13d6ee4d580
Casey Rodarmor <casey@rodarmor.com>
2024-03-30 16:37:05 -0700

    Amend

--- crates/ordinals/src/cenotaph.rs
+++ crates/ordinals/src/cenotaph.rs
@@ -2,9 +2,9 @@ use super::*;
 
 #[derive(Serialize, Eq, PartialEq, Deserialize, Debug, Default)]
 pub struct Cenotaph {
+  pub etching: Option<Rune>,
   pub flaws: u32,
   pub mint: Option<RuneId>,
-  pub etching: Option<Rune>,
 }
 
 impl Cenotaph {
