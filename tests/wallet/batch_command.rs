51a3079eeffc25e59955d0022fa2427efb368e63
raphjaph <raphjaph@protonmail.com>
2024-04-18 01:08:41 -0400

    Amend

--- tests/wallet/batch_command.rs
+++ tests/wallet/batch_command.rs
@@ -2744,41 +2744,36 @@ fn forbid_etching_below_rune_activation_height() {
 
 #[test]
 fn allow_etching_below_rune_activation_height() {
-  let core = mockcore::builder().build();
+  let core = mockcore::builder().network(Network::Regtest).build();
 
-  let ord = TestServer::spawn_with_server_args(&core, &["--index-runes"], &[]);
+  let ord = TestServer::spawn_with_server_args(&core, &["--regtest", "--index-runes"], &[]);
 
   create_wallet(&core, &ord);
 
   core.mine_blocks(1);
 
-  CommandBuilder::new("--index-runes wallet batch --fee-rate 0 --batch batch.yaml")
-    .write("inscription.txt", "foo")
-    .write(
-      "batch.yaml",
-      serde_yaml::to_string(&batch::File {
-        disable_etch_checks: true,
-        etching: Some(batch::Etching {
-          divisibility: 0,
-          rune: SpacedRune {
-            rune: Rune(RUNE),
-            spacers: 0,
-          },
-          supply: "1".parse().unwrap(),
-          premine: "1".parse().unwrap(),
-          symbol: '¢',
-          terms: None,
-          turbo: false,
-        }),
-        inscriptions: vec![batch::Entry {
-          file: Some("inscription.txt".into()),
-          ..default()
-        }],
+  batch(
+    &core,
+    &ord,
+    batch::File {
+      disable_etch_checks: true,
+      etching: Some(batch::Etching {
+        divisibility: 0,
+        rune: SpacedRune {
+          rune: Rune(RUNE),
+          spacers: 0,
+        },
+        supply: "1".parse().unwrap(),
+        premine: "1".parse().unwrap(),
+        symbol: '¢',
+        terms: None,
+        turbo: false,
+      }),
+      inscriptions: vec![batch::Entry {
+        file: Some("inscription.txt".into()),
         ..default()
-      })
-      .unwrap(),
-    )
-    .core(&core)
-    .ord(&ord)
-    .run_and_extract_stdout();
+      }],
+      ..default()
+    },
+  );
 }
