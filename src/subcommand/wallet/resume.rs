036ef6880a6532929c2f57dac6fd12632e07c895
Felipe Lincoln <mail.felipelincoln@gmail.com>
2024-04-19 15:37:46 -0300

    Add `dry-run` flag to `resume` command (#3592)

--- src/subcommand/wallet/resume.rs
+++ src/subcommand/wallet/resume.rs
@@ -4,28 +4,47 @@ use super::*;
 pub struct ResumeOutput {
   pub etchings: Vec<batch::Output>,
 }
+#[derive(Debug, Parser)]
+pub(crate) struct Resume {
+  #[arg(long, help = "Don't broadcast transactions.")]
+  pub(crate) dry_run: bool,
+}
 
-pub(crate) fn run(wallet: Wallet) -> SubcommandResult {
-  let mut etchings = Vec::new();
-  loop {
-    if SHUTTING_DOWN.load(atomic::Ordering::Relaxed) {
-      break;
-    }
+impl Resume {
+  pub(crate) fn run(self, wallet: Wallet) -> SubcommandResult {
+    let mut etchings = Vec::new();
+    loop {
+      if SHUTTING_DOWN.load(atomic::Ordering::Relaxed) {
+        break;
+      }
 
-    for (rune, entry) in wallet.pending_etchings()? {
-      if wallet.is_mature(&entry.commit)? {
-        etchings.push(wallet.send_etching(rune, &entry)?);
+      for (rune, entry) in wallet.pending_etchings()? {
+        if self.dry_run {
+          etchings.push(batch::Output {
+            reveal_broadcast: false,
+            ..entry.output.clone()
+          });
+          continue;
+        };
+
+        if wallet.is_mature(&entry.commit)? {
+          etchings.push(wallet.send_etching(rune, &entry)?);
+        }
+      }
+
+      if wallet.pending_etchings()?.is_empty() {
+        break;
+      }
+
+      if self.dry_run {
+        break;
+      }
+
+      if !wallet.integration_test() {
+        thread::sleep(Duration::from_secs(5));
       }
     }
 
-    if wallet.pending_etchings()?.is_empty() {
-      break;
-    }
-
-    if !wallet.integration_test() {
-      thread::sleep(Duration::from_secs(5));
-    }
+    Ok(Some(Box::new(ResumeOutput { etchings }) as Box<dyn Output>))
   }
-
-  Ok(Some(Box::new(ResumeOutput { etchings }) as Box<dyn Output>))
 }
