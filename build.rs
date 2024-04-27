a0f2e616a366ce7775bbd6402f9270cb87193d66
Linden <70739041+linden@users.noreply.github.com>
2024-04-27 16:56:27 -0700

    fix: link `resolv` on macOS needed for Go's networking

--- build.rs
+++ build.rs
@@ -48,4 +48,8 @@ fn main() {
 
   println!("cargo:rustc-link-search=native=./target");
   println!("cargo:rustc-link-lib=static=tap");
+
+  if cfg!(target_os = "macos") {
+    println!("cargo:rustc-link-lib=resolv");
+  }
 }
