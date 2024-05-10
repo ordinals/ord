d1e14f0c9d4a0e837f5a091d62543ce34555ec16
nghuyenthevinh2000 <nghuyenthevinh@gmail.com>
2024-05-10 13:12:28 +0800

    correct remove previous outpoint in rune_id_to_outpoints_balance

--- src/index/testing.rs
+++ src/index/testing.rs
@@ -134,17 +134,23 @@ impl Context {
 
     pretty_assert_eq!(balances, self.index.get_rune_balances().unwrap());
 
+    // convert from outpoint -> <rune, balance> to rune -> <outpoint, balance>
+    let mut rune_outpoints_balance: HashMap<RuneId, BTreeMap<OutPoint, u128>> = HashMap::new();
     balances
       .iter()
-      .for_each(|rune| {
-        for r in rune.1.iter() {
-          let mut balance: BTreeMap<OutPoint, u128> = BTreeMap::new();
-          *balance
-            .entry(rune.0)
+      .for_each(|outpoint| {
+        for r in outpoint.1.iter() {
+          *rune_outpoints_balance
+            .entry(r.0)
+            .or_default()
+            .entry(outpoint.0)
             .or_default() += r.1;
-          pretty_assert_eq!(balance, self.index.get_rune_specific_balances(self.index.get_rune_by_id(r.0).unwrap().unwrap()).unwrap());
         }
       });
+
+    for i in rune_outpoints_balance {
+      pretty_assert_eq!(i.1, self.index.get_rune_specific_balances(self.index.get_rune_by_id(i.0).unwrap().unwrap()).unwrap());
+    }
 
     let mut outstanding: HashMap<RuneId, u128> = HashMap::new();
 
--- src/index/updater/rune_updater.rs
+++ src/index/updater/rune_updater.rs
@@ -490,6 +490,11 @@ impl<'a, 'tx, 'client> RuneUpdater<'a, 'tx, 'client> {
           let ((id, balance), len) = Index::decode_rune_balance(&buffer[i..]).unwrap();
           i += len;
           *unallocated.entry(id).or_default() += balance;
+
+          let mut outpoints_balance_buffer: Vec<u8> = Vec::new();
+          Index::encode_rune_outpoints_balance(input.previous_output, balance, &mut outpoints_balance_buffer);
+          // this is supposed to have no error as rune_id_to_outpoints_balance should have the outpoint like in outpoint_to_balances
+          let _ = self.rune_id_to_outpoints_balance.remove(id.store(), outpoints_balance_buffer.as_slice());
         }
       }
     }
