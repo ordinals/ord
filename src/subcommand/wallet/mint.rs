3a15393b1b8995f136bc030954dacaf6f025c102
gmart7t2 <49558347+gmart7t2@users.noreply.github.com>
2024-05-15 10:26:01 -0300

    Fix typo in mint.rs
    Postage should be allowed to be equal to the dust limit, just not smaller than it.

--- src/subcommand/wallet/mint.rs
+++ src/subcommand/wallet/mint.rs
@@ -53,7 +53,7 @@ impl Mint {
     };
 
     ensure!(
-      destination.script_pubkey().dust_value() < postage,
+      destination.script_pubkey().dust_value() <= postage,
       "postage below dust limit of {}sat",
       destination.script_pubkey().dust_value().to_sat()
     );
