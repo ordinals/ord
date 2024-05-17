8b83b741d34784587b056f2060396395c7422564
Luis Aguilar <ldiego08@gmail.com>
2024-05-17 09:28:00 -0600

    Add command to list pending etchings (#3732)

---
+++ src/subcommand/wallet/pending.rs
@@ -0,0 +1,28 @@
+use super::*;
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+pub struct PendingOutput {
+  pub commit: Txid,
+  pub rune: SpacedRune,
+}
+#[derive(Debug, Parser)]
+pub(crate) struct Pending {}
+
+impl Pending {
+  pub(crate) fn run(self, wallet: Wallet) -> SubcommandResult {
+    let etchings = wallet
+      .pending_etchings()?
+      .into_iter()
+      .map(|(_, entry)| {
+        let spaced_rune = entry.output.rune.unwrap().rune;
+
+        PendingOutput {
+          rune: spaced_rune,
+          commit: entry.commit.txid(),
+        }
+      })
+      .collect::<Vec<PendingOutput>>();
+
+    Ok(Some(Box::new(etchings) as Box<dyn Output>))
+  }
+}
