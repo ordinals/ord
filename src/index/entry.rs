fb4c6085b99e29abd4b5408571d499f904fac077
nine <118634361+cryptoni9n@users.noreply.github.com>
2024-05-22 14:54:24 -0400

    add max_supply function to entry.rs
    added fn max_supply

--- src/index/entry.rs
+++ src/index/entry.rs
@@ -92,6 +92,16 @@ impl RuneEntry {
           .unwrap_or_default()
   }
 
+    pub fn max_supply(&self) -> u128 {
+    let cap = self.terms.map_or(0, |terms| terms.cap.unwrap_or_default());
+    self.premine
+      + cap
+        * self
+          .terms
+          .and_then(|terms| terms.amount)
+          .unwrap_or_default()
+  }
+
   pub fn pile(&self, amount: u128) -> Pile {
     Pile {
       amount,
