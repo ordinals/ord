81472e8f83aae97388a8e97ad36fdcd6ea624ee1
nine <cryptoni9n@gmail.com>
2024-05-24 14:28:21 -0400

    update drop function to handle panic

--- src/subcommand/env.rs
+++ src/subcommand/env.rs
@@ -4,12 +4,13 @@ struct KillOnDrop(process::Child);
 
 impl Drop for KillOnDrop {
   fn drop(&mut self) {
-    assert!(Command::new("kill")
+    let _ = Command::new("kill")
       .arg(self.0.id().to_string())
-      .status()
-      .unwrap()
-      .success());
-    self.0.wait().unwrap();
+      .status();
+
+    let _ = self.0.kill();
+
+    let _ = self.0.wait();
   }
 }
 
