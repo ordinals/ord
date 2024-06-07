f1ab77d49d3477b1a983984f65bf0f177bc92fcc
raphjaph <raphjaph@protonmail.com>
2024-06-07 17:14:37 +0200

    Add sat balance to address page

--- src/templates/address.rs
+++ src/templates/address.rs
@@ -4,6 +4,7 @@ use super::*;
 pub(crate) struct AddressHtml {
   pub(crate) address: Address,
   pub(crate) outputs: Vec<OutPoint>,
+  pub(crate) sat_balance: u64,
 }
 
 impl PageContent for AddressHtml {
@@ -27,13 +28,20 @@ mod tests {
         .require_network(Network::Bitcoin)
         .unwrap(),
         outputs: vec![outpoint(1), outpoint(2)],
+        sat_balance: 99,
       },
       "<h1>Address bc1phuq0vkls6w926zdaem6x9n02z2gg7j2xfudgwddyey7uyquarlgsh40ev8</h1>
-<h2>Outputs</h2>
-<ul>
-  <li><a class=monospace href=/output/1{64}:1>1{64}:1</a></li>
-  <li><a class=monospace href=/output/2{64}:2>2{64}:2</a></li>
-</ul>.*"
+<dl>
+  <dt>sat balance</dt>
+  <dd>99</dd>
+  <dt>outputs</dt>
+  <dd>
+    <ul>
+      <li><a class=monospace href=/output/1{64}:1>1{64}:1</a></li>
+      <li><a class=monospace href=/output/2{64}:2>2{64}:2</a></li>
+    </ul>
+  </dd>
+</dl>.*"
         .unindent()
     );
   }
