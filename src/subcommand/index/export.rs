84147e15bc1d20d4b40afb4414e55997b4ef564f
popcnt <ism3y@proton.me>
2024-06-26 14:09:52 +0800

    typo

--- src/subcommand/index/export.rs
+++ src/subcommand/index/export.rs
@@ -4,10 +4,10 @@ use super::*;
 pub(crate) struct Export {
   #[arg(long, help = "Write export to <output>")]
   output: String,
-  #[arg(long, help = "Export sequence number > <gt_sequnce>")]
-  gt_sequnce: u32,
-  #[arg(long, help = "Export sequence number < <lt_sequnce>")]
-  lt_sequnce: u32,
+  #[arg(long, help = "Export sequence number > <gt_sequence>")]
+  gt_sequence: u32,
+  #[arg(long, help = "Export sequence number < <lt_sequence>")]
+  lt_sequence: u32,
 }
 
 impl Export {
@@ -15,7 +15,7 @@ impl Export {
     let index = Index::open(&settings)?;
 
     index.update()?;
-    index.export(&self.output, self.gt_sequnce, self.lt_sequnce)?;
+    index.export(&self.output, self.gt_sequence, self.lt_sequence)?;
 
     Ok(None)
   }
