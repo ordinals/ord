c5f4ab5f19f48a1d327636c24e41584c4a125cb1
raph <raphjaph@protonmail.com>
2024-09-14 06:42:41 +0200

    Skip serializing None in batch::File (#3943)

--- src/wallet/batch/file.rs
+++ src/wallet/batch/file.rs
@@ -1,17 +1,18 @@
 use super::*;
 
+#[serde_with::skip_serializing_none]
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone, Default)]
 #[serde(deny_unknown_fields)]
 pub struct File {
-  pub inscriptions: Vec<Entry>,
   pub mode: Mode,
   pub parent: Option<InscriptionId>,
   pub postage: Option<u64>,
   #[serde(default)]
   pub reinscribe: bool,
-  pub etching: Option<batch::Etching>,
   pub sat: Option<Sat>,
   pub satpoint: Option<SatPoint>,
+  pub inscriptions: Vec<batch::entry::Entry>,
+  pub etching: Option<batch::Etching>,
 }
 
 impl File {
