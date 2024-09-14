da8f8a53a9f1f6126afe269c169081cc29ba6caa
raphjaph <raphjaph@protonmail.com>
2024-09-14 08:37:30 +0400

    Amend

--- src/wallet/batch/etching.rs
+++ src/wallet/batch/etching.rs
@@ -4,11 +4,11 @@ use super::*;
 #[derive(Serialize, Deserialize, PartialEq, Debug, Copy, Clone, Default)]
 #[serde(deny_unknown_fields)]
 pub struct Etching {
-  pub divisibility: u8,
-  pub premine: Decimal,
   pub rune: SpacedRune,
-  pub supply: Decimal,
   pub symbol: char,
+  pub divisibility: u8,
+  pub supply: Decimal,
+  pub premine: Decimal,
   pub terms: Option<batch::Terms>,
   pub turbo: bool,
 }
