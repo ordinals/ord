f71a77ec05e7fc9c2d048aa59b2681a6ebd2a06a
raphjaph <raphjaph@protonmail.com>
2024-09-20 21:08:27 +0800

    Amend

--- tests/wallet/send.rs
+++ tests/wallet/send.rs
@@ -362,9 +362,9 @@ inscriptions:
       indexed: true,
       runes: BTreeMap::new(),
       sat_ranges: Some(vec![(5_000_000_000, 5_000_030_000)]),
-      script_pubkey: destination.payload.script_pubkey().to_asm_string(),
+      script_pubkey: destination.payload.script_pubkey(),
       spent: false,
-      transaction: reveal_txid.to_string(),
+      transaction: reveal_txid,
       value: 30_000,
     }
   );
