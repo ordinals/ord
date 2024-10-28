448e76f130146fa476639f397043411512836652
Casey Rodarmor <casey@rodarmor.com>
2024-10-28 13:47:18 -0700

    Tweak

--- src/subcommand/wallet/shared_args.rs
+++ src/subcommand/wallet/shared_args.rs
@@ -18,7 +18,7 @@ pub(super) struct SharedArgs {
   #[arg(
     long,
     alias = "nolimit",
-    help = "Allow transactions larger than MAX_STANDARD_TX_WEIGHT of 400,000 weight units, and \
+    help = "Allow transactions larger than MAX_STANDARD_TX_WEIGHT of 400,000 weight units and \
     OP_RETURNs greater than 83 bytes. Transactions over this limit are nonstandard and will not be \
     relayed by bitcoind in its default configuration. Do not use this flag unless you understand \
     the implications."
