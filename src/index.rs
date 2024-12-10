b8b7c53688e7c020c992371f5afe0fa50db13153
Casey Rodarmor <casey@rodarmor.com>
2024-12-10 14:14:47 -0800

    Bump schema version

--- src/index.rs
+++ src/index.rs
@@ -50,7 +50,7 @@ mod utxo_entry;
 #[cfg(test)]
 pub(crate) mod testing;
 
-const SCHEMA_VERSION: u64 = 29;
+const SCHEMA_VERSION: u64 = 30;
 
 define_multimap_table! { SAT_TO_SEQUENCE_NUMBER, u64, u32 }
 define_multimap_table! { SEQUENCE_NUMBER_TO_CHILDREN, u32, u32 }
