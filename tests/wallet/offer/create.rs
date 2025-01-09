ac16de61392bc46ec189776232e7770892f8329e
Casey Rodarmor <casey@rodarmor.com>
2025-01-09 11:28:34 -0800

    Start working on create success test

--- tests/wallet/offer/create.rs
+++ tests/wallet/offer/create.rs
@@ -1,6 +1,41 @@
 use super::*;
 
 #[test]
+fn created_offer_is_correct() {
+  // todo:
+  // - inscription_must_be_sent_to_wallet_change_output
+  // - inscription_output_must_be_same_size_as_inscription_input
+  // - payment_output_amount_must_include_price_and_postage
+  // - payment_input_is_signed
+  // - payment_output_uses_inscription_address
+  // - psbt_must_use_fee_rate_argument
+
+  let core = mockcore::spawn();
+
+  let ord = TestServer::spawn_with_server_args(&core, &[], &[]);
+
+  create_wallet(&core, &ord);
+
+  let (inscription, _) = inscribe(&core, &ord);
+
+  CommandBuilder::new(format!(
+    "wallet send --fee-rate 1 bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4 {inscription}"
+  ))
+  .core(&core)
+  .ord(&ord)
+  .run_and_deserialize_output::<Send>();
+
+  core.mine_blocks(1);
+
+  CommandBuilder::new(format!(
+    "wallet offer create --inscription {inscription} --amount 1btc --fee-rate 1"
+  ))
+  .core(&core)
+  .ord(&ord)
+  .run_and_extract_stdout();
+}
+
+#[test]
 fn inscription_must_exist() {
   let core = mockcore::spawn();
 
@@ -72,31 +107,7 @@ fn inscription_must_have_valid_address() {
 
 #[test]
 #[ignore]
-fn inscription_must_be_bound() {
-  todo!()
-}
-
-#[test]
-#[ignore]
 fn inscription_must_not_share_output_with_other_inscriptions() {
-  todo!()
-}
-
-#[test]
-#[ignore]
-fn inscription_must_be_sent_to_wallet_change_output() {
-  todo!()
-}
-
-#[test]
-#[ignore]
-fn inscription_output_must_be_same_size_as_inscription_input() {
-  todo!()
-}
-
-#[test]
-#[ignore]
-fn payment_output_amount_must_include_price_and_postage() {
   todo!()
 }
 
@@ -108,18 +119,6 @@ fn non_cardinal_outputs_are_not_selected() {
 
 #[test]
 #[ignore]
-fn payment_input_is_signed() {
-  todo!()
-}
-
-#[test]
-#[ignore]
-fn payment_output_uses_inscription_address() {
-  todo!()
-}
-
-#[test]
-#[ignore]
-fn psbt_must_use_fee_rate_argument() {
+fn inscription_must_be_bound() {
   todo!()
 }
