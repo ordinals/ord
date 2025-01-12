164e896ef5581ea922bb518e2a5e2f2bff37a467
Casey Rodarmor <casey@rodarmor.com>
2025-01-12 15:30:22 -0800

    Add some more busted code

--- src/satscard.rs
+++ src/satscard.rs
@@ -80,21 +80,51 @@ pub(crate) enum AddressRecoveryError {
   Input,
 }
 
-#[derive(Debug, Deserialize, PartialEq)]
-pub(crate) struct Query {
+#[derive(Deserialize)]
+struct QueryInner {
   #[serde(rename = "u")]
-  pub(crate) state: State,
+  state: State,
   #[serde(rename = "o")]
-  pub(crate) slot: u8,
+  slot: u8,
   #[serde(rename = "r")]
-  pub(crate) address_suffix: String,
+  address_suffix: String,
   #[serde(rename = "n", with = "SerHex::<Strict>")]
-  pub(crate) nonce: [u8; 8],
+  nonce: [u8; 8],
   #[serde(rename = "s")]
-  pub(crate) signature: Signature,
+  signature: Signature,
 }
 
-impl Query {
+#[derive(Debug, PartialEq)]
+pub(crate) struct Query {
+  pub(crate) address: Address,
+  pub(crate) address_suffix: String,
+  pub(crate) nonce: [u8; 8],
+  pub(crate) signature: Signature,
+  pub(crate) slot: u8,
+  pub(crate) state: State,
+}
+
+impl<'de> Deserialize<'de> for Query {
+  fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+  where
+    D: Deserializer<'de>,
+  {
+    let inner = QueryInner::deserialize(deserializer)?;
+
+    let address = inner.address().unwrap();
+
+    Ok(Self {
+      address,
+      address_suffix: inner.address_suffix,
+      nonce: inner.nonce,
+      signature: inner.signature,
+      slot: inner.slot,
+      state: inner.state,
+    })
+  }
+}
+
+impl QueryInner {
   // todo: make this infallible? refuse to deserialize if address recovery fails?
   //
   // see: https://github.com/coinkite/coinkite-tap-proto/blob/ed4d8cc46ce370f573705023594282f6faca977e/cktap/utils.py#L307
@@ -118,48 +148,51 @@ impl Query {
 
     let signature = self.signature.0.serialize_compact();
 
-    let mut msg = Vec::<u8>::new();
-    msg.extend(b"OPENDIME");
+    let mut msg = b"OPENDIME".to_vec();
 
-    // card nonce
+    // card nonce???
     msg.extend(hex::encode(&self.nonce).bytes());
 
-    // user nonce
+    // user nonce???
     // msg.extend(hex::encode(&self.nonce).bytes());
     // msg.extend([0; USER_NONCE_SIZE]);
 
     msg.push(self.slot); // todo: I think this is correct but need to verify
 
-    // assert_eq!(msg.len(), 8 + CARD_NONCE_SIZE + USER_NONCE_SIZE + 1);
+    assert_eq!(msg.len(), 8 + CARD_NONCE_SIZE + USER_NONCE_SIZE + 1);
 
     let digest = bitcoin::hashes::sha256::Hash::hash(&msg);
 
     let message = Message::from_digest(*digest.as_ref());
 
-    for id in 0.. {
-      let Ok(id) = RecoveryId::from_i32(id) else {
+    for i in 0.. {
+      eprintln!("trying recovery ID {i}");
+
+      let Ok(id) = RecoveryId::from_i32(i) else {
         break;
       };
 
-      let Ok(signature) = RecoverableSignature::from_compact(&signature, id) else {
-        continue;
-      };
+      let signature = RecoverableSignature::from_compact(&signature, id).unwrap();
 
-      eprintln!("recovered signature for ID {}", id.to_i32());
+      eprintln!("    recovered signature");
 
       let Ok(public_key) = secp.recover_ecdsa(&message, &signature) else {
+        eprintln!("    recovery failed");
         continue;
       };
 
-      eprintln!("recovered public key for ID {}", id.to_i32());
+      eprintln!("    recovered public key");
 
-      // let Ok(_) = secp.verify_ecdsa(
-      //   &message,
-      //   &secp256k1::ecdsa::Signature::from_compact(&self.signature.0.serialize_compact()).unwrap(),
-      //   &public_key,
-      // ) else {
-      //   continue;
-      // };
+      secp
+        .verify_ecdsa(
+          &message,
+          &secp256k1::ecdsa::Signature::from_compact(&self.signature.0.serialize_compact())
+            .unwrap(),
+          &public_key,
+        )
+        .unwrap();
+
+      eprintln!("    verified signature with recovered public key");
 
       let public_key = PublicKey::new(public_key);
 
@@ -167,11 +200,14 @@ impl Query {
 
       let address = Address::p2wpkh(&public_key, bitcoin::KnownHrp::Mainnet);
 
-      eprintln!("recovered address for ID {}: {}", id.to_i32(), address);
+      eprintln!("    recovered address {address}");
 
       if address.to_string().ends_with(&self.address_suffix) {
+        eprintln!("    address match");
         return Ok(address);
       }
+
+      eprintln!("    address mismatch");
     }
 
     Err(AddressRecoveryError::Input)
@@ -185,14 +221,17 @@ mod tests {
   const TEST_URI: &str =
     "https://getsatscard.com/start#u=S&o=0&r=a5x2tplf&n=7664168a4ef7b8e8&s=42b209c86ab90be6418d36b0accc3a53c11901861b55be95b763799842d403dc17cd1b74695a7ffe2d78965535d6fe7f6aafc77f6143912a163cb65862e8fb53";
 
-  const TEST_ADDRESS: &str = "bc1ql86vqdwylsgmgkkrae5nrafte8yp43a5x2tplf";
-
   fn test_query() -> Query {
     Query {
       state: State::Sealed,
       slot: 0,
       address_suffix: "a5x2tplf".into(),
       nonce: [0x76, 0x64, 0x16, 0x8a, 0x4e, 0xf7, 0xb8, 0xe8],
+      address: "bc1ql86vqdwylsgmgkkrae5nrafte8yp43a5x2tplf"
+        .parse::<Address<NetworkUnchecked>>()
+        .unwrap()
+        .require_network(Network::Bitcoin)
+        .unwrap(),
       signature: Signature(
         secp256k1::ecdsa::Signature::from_compact(
           &hex::decode(TEST_URI.rsplit('=').next().unwrap()).unwrap(),
@@ -210,10 +249,5 @@ mod tests {
       axum::extract::Query::<Query>::try_from_uri(&uri).unwrap().0,
       test_query(),
     );
-  }
-
-  #[test]
-  fn recover_address() {
-    assert_eq!(test_query().address().unwrap().to_string(), TEST_ADDRESS);
   }
 }
