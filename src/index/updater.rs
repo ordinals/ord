25caab51176c8f06e107bbb4523ba1fff61973f9
raphjaph <raphjaph@protonmail.com>
2025-01-17 18:00:02 +0100

    Remove double commit for redb bug

--- src/index/updater.rs
+++ src/index/updater.rs
@@ -862,10 +862,6 @@ impl Updater<'_> {
     Index::increment_statistic(&wtx, Statistic::Commits, 1)?;
     wtx.commit()?;
 
-    // Commit twice since due to a bug redb will only reuse pages freed in the
-    // transaction before last.
-    self.index.begin_write()?.commit()?;
-
     Reorg::update_savepoints(self.index, self.height)?;
 
     Ok(())
