d2431ffedd22979deeba392841989ed84e0a4fa9
raphjaph <raphjaph@protonmail.com>
2025-03-01 20:48:52 +0100

    Amend

--- src/wallet/database.rs
+++ src/wallet/database.rs
@@ -37,26 +37,13 @@ impl WalletPersister for DatabasePersister {
   type Error = Error;
 
   fn initialize(persister: &mut Self) -> std::result::Result<ChangeSet, Self::Error> {
-    let rtx = persister.0.begin_read()?;
-
-    let changeset = match rtx
-      .open_table(CHANGESET)?
-      .get(())?
-      .map(|result| result.value().to_string())
-    {
-      Some(result) => serde_json::from_str::<ChangeSet>(result.as_str())?,
-      None => ChangeSet::default(),
-    };
-
-    Ok(changeset)
+    TransactionPersister::initialize(&mut TransactionPersister(&mut persister.0.begin_write()?))
   }
 
   fn persist(persister: &mut Self, changeset: &ChangeSet) -> std::result::Result<(), Self::Error> {
-    let wtx = persister.0.begin_write()?;
+    let mut wtx = persister.0.begin_write()?;
 
-    wtx
-      .open_table(CHANGESET)?
-      .insert((), serde_json::to_string(changeset)?.as_str())?;
+    TransactionPersister::persist(&mut TransactionPersister(&mut wtx), changeset)?;
 
     wtx.commit()?;
 
