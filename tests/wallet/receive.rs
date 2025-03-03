cd5b973e4d86d1157dcf04bef6e035d67f8169b1
raph <raphjaph@protonmail.com>
2025-03-03 19:55:30 +0100

    Migrate create, restore, and receive to bdk (#4231)

--- tests/wallet/receive.rs
+++ tests/wallet/receive.rs
@@ -5,16 +5,25 @@ fn receive() {
   let core = mockcore::spawn();
   let ord = TestServer::spawn(&core);
 
-  create_wallet(&core, &ord);
+  ord.create_wallet(&core);
 
   let output = CommandBuilder::new("wallet receive")
+    .temp_dir(ord.tempdir().clone())
     .core(&core)
     .ord(&ord)
     .run_and_deserialize_output::<receive::Output>();
 
-  assert!(output
-    .addresses
-    .first()
-    .unwrap()
-    .is_valid_for_network(Network::Bitcoin));
+  let first_address = output.addresses.first().unwrap();
+
+  assert!(first_address.is_valid_for_network(Network::Bitcoin));
+
+  let output = CommandBuilder::new("wallet receive")
+    .temp_dir(ord.tempdir().clone())
+    .core(&core)
+    .ord(&ord)
+    .run_and_deserialize_output::<receive::Output>();
+
+  let second_address = output.addresses.first().unwrap();
+
+  assert!(second_address != first_address);
 }
