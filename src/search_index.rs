0c9f16bff7d0a36b4c705d5dc8b2d492ebe615ea
Liam <liam@scalzulli.com>
2025-04-01 14:05:06 -0400

    Format

--- src/search_index.rs
+++ src/search_index.rs
@@ -151,7 +151,6 @@ impl SearchIndex {
           indexed_inscriptions.push(inscription_id);
         }
 
-
         if SHUTTING_DOWN.load(atomic::Ordering::Relaxed) {
           return Ok(());
         }
