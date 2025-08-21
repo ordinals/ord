0cb5131db5d0b7694f238dffd8fc607aa27421cd
raphjaph <raphjaph@protonmail.com>
2025-08-21 12:17:54 -0700

    Write test

--- tests/wallet/sweep.rs
+++ tests/wallet/sweep.rs
@@ -13,39 +13,48 @@ fn sweepable_address() -> (Address, String) {
 
   let address = Address::p2wpkh(&sk.public_key(&secp).try_into().unwrap(), Network::Bitcoin);
 
-  let descriptor = format!("wpkh({})", sk.to_wif());
-
-  (address, descriptor)
+  (address, sk.to_wif())
 }
 
 #[test]
 fn sweep() {
   let core = mockcore::spawn();
-  let ord = TestServer::spawn_with_server_args(&core, &["--index-addresses"], &[]);
+  let ord = TestServer::spawn_with_server_args(&core, &["--index-addresses", "--index-runes"], &[]);
 
   create_wallet(&core, &ord);
 
   let (inscription, _) = inscribe(&core, &ord);
 
-  let (address, descriptor) = sweepable_address();
+  let (address, wif_privkey) = sweepable_address();
 
-  let _output = CommandBuilder::new(format!("wallet send --fee-rate 1 {address} {inscription}",))
+  CommandBuilder::new(format!("wallet send --fee-rate 1 {address} {inscription}",))
     .core(&core)
     .ord(&ord)
     .stdout_regex(r".*")
     .run_and_deserialize_output::<Send>();
 
-  // TODO: assert successful transfer
-
   core.mine_blocks(1);
 
-  let _output = CommandBuilder::new("wallet sweep --fee-rate 1")
-    .stdin(descriptor.into())
+  let output = CommandBuilder::new("wallet inscriptions")
     .core(&core)
     .ord(&ord)
+    .run_and_deserialize_output::<Inscriptions>();
+
+  assert!(output.is_empty());
+
+  CommandBuilder::new("wallet sweep --fee-rate 1 --address-type p2wpkh")
+    .stdin(wif_privkey.into())
+    .core(&core)
+    .ord(&ord)
+    .stderr_regex(".*")
     .run_and_deserialize_output::<Sweep>();
 
   core.mine_blocks(1);
 
-  // TODO: assert successful sweep
+  let output = CommandBuilder::new("wallet inscriptions")
+    .core(&core)
+    .ord(&ord)
+    .run_and_deserialize_output::<Inscriptions>();
+
+  assert_eq!(output[0].inscription, inscription);
 }
