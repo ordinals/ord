69a9c92f6be34f6acf2d0fcccba17420a9f0af63
Jon S. <totalwine2337@gmail.com>
2025-09-25 16:08:56 -0500

    Update and rename config.py to conf.py

---
+++ docs/conf.py
@@ -0,0 +1,12 @@
+# docs/conf.py
+project = 'Ord'
+author = 'Pray4Love1'
+release = '0.1'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
