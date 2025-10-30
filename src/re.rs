60652a7f3d97816bf0e35b813ec271726b300fae
gmart7t2 <49558347+gmart7t2@users.noreply.github.com>
2025-10-30 16:18:07 -0300

    Fix redirects for outpoints and TXIDs consisting only of decimal digits (#4421)

--- src/re.rs
+++ src/re.rs
@@ -12,11 +12,11 @@ lazy_static! {
     re(r"https://(get)?satscard.com/start#(?<parameters>.*)");
   pub(crate) static ref HASH: Regex = re(r"[[:xdigit:]]{64}");
   pub(crate) static ref INSCRIPTION_ID: Regex = re(r"[[:xdigit:]]{64}i\d+");
-  pub(crate) static ref INSCRIPTION_NUMBER: Regex = re(r"-?[0-9]+");
+  pub(crate) static ref INSCRIPTION_NUMBER: Regex = re(r"-?[0-9]{1,63}");
   pub(crate) static ref ORDINALS_SATSCARD_URL: Regex =
     re(r"https://ordinals.com/satscard\?(?<query>.*)");
   pub(crate) static ref OUTPOINT: Regex = re(r"[[:xdigit:]]{64}:\d+");
-  pub(crate) static ref RUNE_ID: Regex = re(r"[0-9]+:[0-9]+");
+  pub(crate) static ref RUNE_ID: Regex = re(r"[0-9]{1,63}:[0-9]+");
   pub(crate) static ref RUNE_NUMBER: Regex = re(r"-?[0-9]+");
   pub(crate) static ref SATPOINT: Regex = re(r"[[:xdigit:]]{64}:\d+:\d+");
   pub(crate) static ref SAT_NAME: Regex = re(r"[a-z]{1,11}");
