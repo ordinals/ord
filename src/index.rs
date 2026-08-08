293da3e1c83f697f81d8e9a8ebc52213ed7484f2
Bortlesboat <169967362+Bortlesboat@users.noreply.github.com>
2026-08-08 03:03:44 -0400

    Warn when exporting addresses without address index

--- src/index.rs
+++ src/index.rs
@@ -707,6 +707,13 @@ impl Index {
   }
 
   pub fn export(&self, filename: &String, include_addresses: bool) -> Result {
+    if include_addresses && !self.index_addresses {
+      eprintln!(
+        "warning: exporting addresses without `--index-addresses` requires fetching every \
+         transaction over RPC, which is slow"
+      );
+    }
+
     let mut writer = BufWriter::new(File::create(filename)?);
     let rtx = self.database.begin_read()?;
 
